660eea69707bc08e262ae5c4832444f0d3ec94e3
AlexanderTheGr <a.dimeas@gmail.com>
2015-10-09 17:13:35 +0300

    ...

--- src/EavBundle/EventListener/EavAttributeCreatorListener.php
+++ src/EavBundle/EventListener/EavAttributeCreatorListener.php
@@ -9,32 +9,30 @@ use Doctrine\ORM\UnitOfWork;
 use EavBundle\Entity\EavAttribute;
 use EavBundle\Entity\EavEntity;
 
-class EavAttributeCreatorListener
-{
-    private $entities = null;
+class EavAttributeCreatorListener {
 
+    private $entities = null;
     private $cacheDir;
 
-    public function __construct($cacheDir)
-    {
+    public function __construct($cacheDir) {
         $this->cacheDir = $cacheDir;
     }
 
-    public function postLoad(LifecycleEventArgs $eventArgs)
-    {
-        
-        echo "s";
+    public function postPersist(LifecycleEventArgs $eventArgs) {
+
+
         $em = $eventArgs->getEntityManager();
         $uow = $em->getUnitOfWork();
         $entity = $eventArgs->getEntity();
         $classname = get_class($entity);
+
 
         if (!array_key_exists($classname, $this->getEntities())) {
             return null;
         }
 
         /** @var EavEntity $eavEntity */
-        $eavEntity = $em->getRepository('EavEntity:EavEntity')->findOneBy([
+        $eavEntity = $em->getRepository('EavBundle:EavEntity')->findOneBy([
             'entity' => $classname,
         ]);
 
@@ -42,27 +40,6 @@ class EavAttributeCreatorListener
             throw new \UnexpectedValueException('EavEntity not found for ' . $classname);
         }
 
-        $qb = $em->getRepository($classname)->createQueryBuilder('main');
-
-        $qb
-            ->distinct()
-            ->select('d.id')
-            ->join('main.attributes', 'a')
-            ->join('a.definition', 'd', null, null, 'd.id')
-            ->where('main = :main')
-            ->setParameter('main', $entity)
-        ;
-
-        $definitions = $qb->getQuery()->getArrayResult();
-
-        foreach ($eavEntity->getDefinitions() as $definition) {
-            if (!array_key_exists($definition->getId(), $definitions)) {
-                $attribute = new EavAttribute();
-                $attribute->setDefinition($definition);
-
-                $entity->addEavAttribute($attribute);
-            }
-        }
 
         if ($uow->getEntityState($entity) == UnitOfWork::STATE_MANAGED) {
             $em->persist($entity);
@@ -73,12 +50,12 @@ class EavAttributeCreatorListener
     /**
      * @return array
      */
-    protected function getEntities()
-    {
+    protected function getEntities() {
         if ($this->entities === null) {
             $this->entities = include $this->cacheDir . '/eav_bundle/Entity.cache.php';
         }
 
         return $this->entities;
     }
+
 }
