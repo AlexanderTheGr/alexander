d4d079589a305c206d5b857b899c93921197f9a0
AlexanderTheGr <a.dimeas@gmail.com>
2015-12-15 17:19:11 +0200

    ...

---
+++ src/EdiBundle/Entity/EltrekaediOrderItem.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace EdiBundle\Entity;
+
+/**
+ * EltrekaediOrderItem
+ */
+class EltrekaediOrderItem
+{
+    /**
+     * @var integer
+     */
+    private $order;
+
+    /**
+     * @var integer
+     */
+    private $product;
+
+    /**
+     * @var integer
+     */
+    private $qty;
+
+    /**
+     * @var string
+     */
+    private $price;
+
+    /**
+     * @var string
+     */
+    private $disc1prc;
+
+    /**
+     * @var string
+     */
+    private $lineval;
+
+    /**
+     * @var integer
+     */
+    private $store = '7021';
+
+    /**
+     * @var boolean
+     */
+    private $chk;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Set order
+     *
+     * @param integer $order
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+
+        return $this;
+    }
+
+    /**
+     * Get order
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * Set product
+     *
+     * @param integer $product
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return integer
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * Set qty
+     *
+     * @param integer $qty
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setQty($qty)
+    {
+        $this->qty = $qty;
+
+        return $this;
+    }
+
+    /**
+     * Get qty
+     *
+     * @return integer
+     */
+    public function getQty()
+    {
+        return $this->qty;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set disc1prc
+     *
+     * @param string $disc1prc
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setDisc1prc($disc1prc)
+    {
+        $this->disc1prc = $disc1prc;
+
+        return $this;
+    }
+
+    /**
+     * Get disc1prc
+     *
+     * @return string
+     */
+    public function getDisc1prc()
+    {
+        return $this->disc1prc;
+    }
+
+    /**
+     * Set lineval
+     *
+     * @param string $lineval
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setLineval($lineval)
+    {
+        $this->lineval = $lineval;
+
+        return $this;
+    }
+
+    /**
+     * Get lineval
+     *
+     * @return string
+     */
+    public function getLineval()
+    {
+        return $this->lineval;
+    }
+
+    /**
+     * Set store
+     *
+     * @param integer $store
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setStore($store)
+    {
+        $this->store = $store;
+
+        return $this;
+    }
+
+    /**
+     * Get store
+     *
+     * @return integer
+     */
+    public function getStore()
+    {
+        return $this->store;
+    }
+
+    /**
+     * Set chk
+     *
+     * @param boolean $chk
+     *
+     * @return EltrekaediOrderItem
+     */
+    public function setChk($chk)
+    {
+        $this->chk = $chk;
+
+        return $this;
+    }
+
+    /**
+     * Get chk
+     *
+     * @return boolean
+     */
+    public function getChk()
+    {
+        return $this->chk;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
