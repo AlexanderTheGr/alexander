062bae38aa5fb75b1e11104036ed04709b9e8ae0
AlexanderTheGr <a.dimeas@gmail.com>
2016-02-05 15:32:34 +0200

    ...

--- src/SoftoneBundle/Controller/DefaultController.php
+++ src/SoftoneBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace SoftoneBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller {
 
@@ -13,17 +17,27 @@ class DefaultController extends Controller {
      */
     public function indexAction(Request $request) {
         // replace this example code with whatever you need
-        
+
         set_time_limit(100000);
         ini_set('memory_limit', '128M');
-        
+
         $kernel = $this->get('kernel');
-        $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
+        $application = new Application($kernel);
         $application->setAutoExit(false);
-        //Create de Schema 
+
         $options = array('command' => 'doctrine:schema:update', "--force" => true);
-        $application->run(new \Symfony\Component\Console\Input\ArrayInput($options));
-        
+        $input = new ArrayInput(array(
+            'command' => 'doctrine:schema:update',
+            "--force" => true
+        ));
+        // You can use NullOutput() if you don't need the output
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        // return the output, don't use if you used NullOutput()
+        $content = $output->fetch();
+
+
         return $this->render('default/index.html.twig', array(
                     'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
         ));
