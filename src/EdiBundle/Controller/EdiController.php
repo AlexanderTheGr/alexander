41bf17ab802826c4b7bf20fc6bb80ec30c144afa
AlexanderTheGr <a.dimeas@gmail.com>
2016-09-20 14:37:54 +0300

    ...

--- src/EdiBundle/Controller/EdiController.php
+++ src/EdiBundle/Controller/EdiController.php
@@ -155,6 +155,7 @@ class EdiController extends Main {
             }
             $i = 0;
             while ($data = fgetcsv($handle, 100000, "\t")) {
+                if ($i++ < 130000) continue;
                 foreach ($data as $key => $val) {
                     $attributes[$attrs[$key]] = trim(addslashes($val));
                 }
