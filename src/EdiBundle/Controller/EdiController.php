635f8581fcc257cdd44eb5e32aef5122f191ce2a
AlexanderTheGr <a.dimeas@gmail.com>
2016-09-20 12:03:52 +0300

    ...

--- src/EdiBundle/Controller/EdiController.php
+++ src/EdiBundle/Controller/EdiController.php
@@ -105,6 +105,7 @@ class EdiController extends Main {
             print_r($attrs);
             $i = 0;
             while ($data = fgetcsv($handle, 1000, "\t")) {
+                if ($i++ == 0) continue;
                 foreach ($data as $key => $val) {
                     $attributes[$attrs[$key]] = $val;
                 }
