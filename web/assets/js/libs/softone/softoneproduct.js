64794a37c1cd1cecb3188bc55f246a8b6bd63f7b
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-01 11:36:35 +0200

    ...

--- web/assets/js/libs/softone/softoneproduct.js
+++ web/assets/js/libs/softone/softoneproduct.js
@@ -4,36 +4,11 @@ jQuery('.synafiacode input').live("keyup", function (e) {
     }
 });
 
-
-var $elem = jQuery(".synafiacode input").autocomplete({
-    source: "/customer/autocompletesearch",
-    method: "POST",
-    minLength: 2,
-    select: function (event, ui) {
-        var data = {}
-        $.post("/product/seachProduct", data, function (result) {
-            if (result.returnurl) {
-                //location.href = result.returnurl;
-            }
-        })
-    }
-})
-
-
-
 var $elem = jQuery(".synafiacode input").autocomplete({
     source: "/product/autocompletesearch",
     method: "POST",
     minLength: 2,
     select: function (event, ui) {
         var data = {}
-        data.id = customerNameArr[3];
-        data.customer = ui.item.id;
-        data.customerName = ui.item.label;
-        $.post("/order/saveCustomer", data, function (result) {
-            if (result.returnurl) {
-                location.href = result.returnurl;
-            }
-        })
     }
 })
