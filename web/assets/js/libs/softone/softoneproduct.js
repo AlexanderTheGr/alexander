9704191d98d1afa7c73eca0914c2cfed2d0dce0d
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-01 11:29:15 +0200

    ...

--- web/assets/js/libs/softone/softoneproduct.js
+++ web/assets/js/libs/softone/softoneproduct.js
@@ -1,8 +1,25 @@
 jQuery('.synafiacode input').live("keyup", function (e) {
     if (e.keyCode == 13) {
-        alert("SSSSS");
+        alert($(this).val());
     }
 });
+
+var $elem = jQuery(".synafiacode input").autocomplete({
+    source: "/customer/autocompletesearch",
+    method: "POST",
+    minLength: 2,
+    select: function (event, ui) {
+        var data = {}
+        data.id = customerNameArr[3];
+        data.customer = ui.item.id;
+        data.customerName = ui.item.label;
+        $.post("/product/seachProduct", data, function (result) {
+            if (result.returnurl) {
+                //location.href = result.returnurl;
+            }
+        })
+    }
+})
 
 
 var $elem = jQuery(".synafiacode input").autocomplete({
