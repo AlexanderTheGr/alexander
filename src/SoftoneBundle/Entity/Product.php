bb855e6589b06e64f669a5b88aa28faf25c94d2f
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-02 15:15:33 +0200

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -1705,7 +1705,7 @@ class Product extends Entity {
     }
 
     function updatetecdoc() {
-        //echo 'sssssssssss';
+        echo 'sssssssssss';
         //$data = array("service" => "login", 'username' => 'dev', 'password' => 'dev', 'appId' => '2000');
         if ($this->getTecdocSupplierId() == null)
             return;
