bea29e1c48213b177e321534f4bb2ad389fdd829
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-03 14:16:29 +0200

    ...

--- src/EdiBundle/Entity/EdiItem.php
+++ src/EdiBundle/Entity/EdiItem.php
@@ -593,7 +593,7 @@ class EdiItem extends Entity {
 
 
         $product->setItemMtrmark($this->dlnr);
-        $product->setTecdocArticleId($this->dlnr);
+        $product->setTecdocSupplierId($this->dlnr);
         $product->setTecdocCode($this->artNr);
         $product->setItemName($this->description);
         $product->setTecdocArticleId($this->tecdocArticleId);
