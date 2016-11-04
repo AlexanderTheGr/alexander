ba8604a80b63a6180210ec3cbbb7c169d9b08d5e
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-04 10:30:36 +0200

    ...

--- src/AppBundle/Controller/Main.php
+++ src/AppBundle/Controller/Main.php
@@ -483,6 +483,7 @@ class Main extends Controller {
         foreach ($fields as $field => $options) {
 
             @$options["type"] = $options["type"] ? $options["type"] : "input";
+            $options["required"] = false;
             if ($options["type"] == 'select') {
                 @$options["required"] = $options["required"] ? $options["required"] : true;
                 $datasource = $options["datasource"];
