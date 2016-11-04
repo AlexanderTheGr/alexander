b727187fa72b1381bd2b13f8ea9a6eb2bc6bce35
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-04 16:15:28 +0200

    ...

--- src/AppBundle/Controller/Main.php
+++ src/AppBundle/Controller/Main.php
@@ -435,13 +435,13 @@ class Main extends Controller {
         
         $em = $this->getDoctrine()->getManager();
         //$this->error[$this->repository] = array();
-        
+        $asd = $entity;
         foreach ($entity->uniques as $attr) {
             $ent = $this->getDoctrine()
                     ->getRepository($this->repository)
                     ->findOneBy(array($attr => $entity->getField($attr)));
             if (count($ent)) {
-                $this->error[$this->repository][] = $this->repository.":".$attr.":".@$entity->id;
+                $this->error[$this->repository][] = $this->repository.":".$attr.":".@$asd->id;
                 $entity = $ent;
             }
         }
