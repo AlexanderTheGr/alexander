ba16df88d275c81db371e10feaf5f27f7764b221
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-04 14:47:26 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -224,7 +224,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         $product->updatetecdoc();
         $product->toSoftone();
 
-        $json = json_encode(array("error" => 0, "id" => (int) $product->id, 'returnurl' => '/product/view/' . (int) $product->id));
+        $json = json_encode(array("error" => 0, "id" => (int) $product->id, 'returnur' => '/product/view/' . (int) $product->id));
         return new Response(
                 $json, 200, array('Content-Type' => 'application/json')
         );
