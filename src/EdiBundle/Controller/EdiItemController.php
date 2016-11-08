c9613c608f2179d44e4e411b33d99a768d703d6f
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-08 16:28:55 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -335,7 +335,7 @@ class EdiItemController extends Main {
                 $this->createWhere();
             }
             
-            echo $this->where."\n\n";
+            //echo $this->where."\n\n";
             $recordsFiltered = $em->getRepository($this->repository)->recordsFiltered($this->where);
 
             $query = $em->createQuery(
