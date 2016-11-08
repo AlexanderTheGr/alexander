5665f7d89628aa9f79ae67966b7177a192dacfb7
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-08 16:34:40 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -328,14 +328,14 @@ class EdiItemController extends Main {
             //print_r($articles["articleIds"]);
             $this->createWhere();
             if (count($articleIds)) {
-                //$edi = $dt_columns[1]["search"]["value"];
+                $edi = $dt_columns[1]["search"]["value"];
                 //$edi = $em->getRepository("EdiBundle:Edi")->find(1);
-                //$this->where = " where p.edi = ".$edi." AND " . $this->prefix . ".tecdocArticleId in (" . (implode(",", $articleIds)) . ")";
+                $this->where = " where p.Edi = ".$edi." AND " . $this->prefix . ".tecdocArticleId in (" . (implode(",", $articleIds)) . ")";
             } else {
-                //$this->createWhere();
+                $this->createWhere();
             }
             
-            echo $this->where."\n\n";
+            //echo $this->where."\n\n";
             $recordsFiltered = $em->getRepository($this->repository)->recordsFiltered($this->where);
 
             $query = $em->createQuery(
