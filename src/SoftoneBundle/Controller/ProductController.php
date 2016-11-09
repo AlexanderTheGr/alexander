4c17791ec0bdd8797029123802a09de6372247eb
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-09 16:12:11 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -494,7 +494,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                     'buttons' => $buttons,
                     'ctrl' => $this->generateRandomString(),
                     'app' => $this->generateRandomString(),
-                    'content' => $this->efarmoges($request->request->get("ref")),
+                    'content' => $this->originals($request->request->get("ref")),
                     'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
         ));
     }   
