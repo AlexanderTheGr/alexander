eb695d76f41b131a51df397d727f331a10ce2d90
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-11 16:33:28 +0200

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -664,6 +664,7 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
         //ADBRP002
         //if ($_SERVER["DOCUMENT_ROOT"] == 'C:\symfony\alexander\webb') {
             
+        
             $url = $this->getSetting("AppBundle:Entity:tecdocServiceUrl");
             $fields = array(
                 'action' => 'getSearch',
