21cab74903d702c5aa783ebe3affbf76022bfd2b
AlexanderTheGr <a.dimeas@gmail.com>
2016-11-14 15:05:58 +0200

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -662,8 +662,8 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
         //   return $data;
         //} else {
         //ADBRP002
-        if ($_SERVER["DOCUMENT_ROOT"] == 'C:\symfony\alexander\webb') {
-            /*
+        //if ($_SERVER["DOCUMENT_ROOT"] == 'C:\symfony\alexander\webb') {
+            
             $url = $this->getSetting("AppBundle:Entity:tecdocServiceUrl");
             $fields = array(
                 'action' => 'getSearch',
@@ -682,8 +682,9 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
             $data = curl_exec($ch);
             return $data;
             //}
-             * 
-             */
+          
+        
+        /*    
         } else {
             $tecdoc = new Tecdoc();
             $articles = $tecdoc->getArticlesSearch(array('search'=>$search));
@@ -692,9 +693,10 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
             foreach ($articles->data->array as $v) {
                 $articleIds[] = $v->articleId;
             }
-            print_r($articleIds);
             return serialize($articleIds);
         }
+         * 
+         */
     }
 
     function getTabContentSearch($order) {
