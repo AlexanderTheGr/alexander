c4992570324546ee8d1479b28a47cd6fdc45047d
AlexanderTheGr <a.dimeas@gmail.com>
2016-12-12 16:08:02 +0200

    ...

--- src/EdiBundle/Controller/EdiController.php
+++ src/EdiBundle/Controller/EdiController.php
@@ -164,7 +164,7 @@ class EdiController extends Main {
     }
 
     public function getEltrekaPartMaster($entity) {
-        //return;
+        return;
         set_time_limit(100000);
         $eltrekaedi = new Eltrekaedi();
         $file = $eltrekaedi->getPartMasterFile();
