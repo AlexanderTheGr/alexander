1c6a75cd0367ac07bb161cc0247f164dabb5a2b5
AlexanderTheGr <a.dimeas@gmail.com>
2016-12-13 16:06:44 +0200

    ...

--- src/SoftoneBundle/Entity/Customergroup.php
+++ src/SoftoneBundle/Entity/Customergroup.php
@@ -3,6 +3,10 @@
 namespace SoftoneBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Entity\Entity;
+use SoftoneBundle\Entity\Softone as Softone;
+
+
 
 /**
  * Customergroup
@@ -10,8 +14,65 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="customergroup")
  * @ORM\Entity
  */
-class Customergroup
-{
+class Customergroup  extends  Entity {
+    var $repositories = array();
+
+    public function __construct() {
+        $this->addresses = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->setRepositories();
+    }
+
+    public function getField($field) {
+
+        return $this->$field;
+    }
+
+    public function setField($field, $val) {
+        $this->$field = $val;
+        return $val;
+    }
+
+    public function setRepositories() {
+        //$this->repositories['tecdocSupplierId'] = 'SoftoneBundle:TecdocSupplier';
+        $this->types['tecdocSupplierId'] = 'object';
+        //$this->tecdocSupplierId = new \SoftoneBundle\Entity\TecdocSupplier;
+    }
+
+    public function getRepository() {
+        return $this->repository;
+    }
+
+    public function getRepositories($repo) {
+        //$this->repositories['tecdocSupplierId'] = 'SoftoneBundle:TecdocSupplier';
+        return $this->repositories[$repo];
+    }
+
+    public function gettype($field) {
+        $this->types['tecdocSupplierId'] = 'object';
+        if (@$this->types[$field] != '') {
+            return @$this->types[$field];
+        }
+        if (gettype($field) != NULL) {
+            return gettype($this->$field);
+        }
+        return 'string';
+    }
+
+    function createName($str) {
+        $strArr = explode("_", $str);
+        $i = 0;
+        $b = "";
+        foreach ($strArr as $a) {
+            $b .= ucfirst($a);
+        }
+        $strArr = explode(".", $b);
+        $i = 0;
+        $b = "";
+        foreach ($strArr as $a) {
+            $b .= ucfirst($a);
+        }
+        return $b;
+    }
     /**
      * @var string
      *
