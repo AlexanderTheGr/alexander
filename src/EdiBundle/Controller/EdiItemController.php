f8f5c7298d6fdf81daa9332bc16286a2c4999fb7
AlexanderTheGr <a.dimeas@gmail.com>
2016-12-15 13:44:22 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -285,7 +285,7 @@ class EdiItemController extends Main {
         $i = 0;
         $tecdoc = new Tecdoc();
         foreach ($results as $result) {
-            if ($result["id"] > 275420) {
+            if ($result["id"] > 287123) {
                 $ediediitem = $em->getRepository($this->repository)->find($result["id"]);
                 $ediediitem->tecdoc = $tecdoc;
                 $ediediitem->updatetecdoc();
