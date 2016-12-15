dddaad2083ef493030d64c0e01806442d6fccfb6
AlexanderTheGr <a.dimeas@gmail.com>
2016-12-15 12:46:29 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -290,8 +290,9 @@ class EdiItemController extends Main {
                 $ediediitem->tecdoc = $tecdoc;
                 $ediediitem->updatetecdoc();
                 unset($ediediitem);
+                echo $result["id"]."<BR>";
             }
-            echo $result["id"]."<BR>";
+            
             if ($i++ > 300) exit;
         }
         exit;
