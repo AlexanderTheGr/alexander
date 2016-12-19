63b4bd539b20fb3184b8560d4479968be6018c42
AlexanderTheGr <a.dimeas@gmail.com>
2016-12-19 12:12:31 +0200

    ...

--- src/SoftoneBundle/Controller/CustomergroupController.php
+++ src/SoftoneBundle/Controller/CustomergroupController.php
@@ -88,7 +88,7 @@ class CustomergroupController extends Main {
 
 
         return $this->render('SoftoneBundle:Customergroup:view.html.twig', array(
-                    'pagename' => $entity->getName(),
+                    'pagename' => $entity->getTitle(),
                     'url' => '/customergroup/save',
                     'buttons' => $buttons,
                     'ctrl' => $this->generateRandomString(),
