29332521d9e4f511eb138ba3582743f5e5ab44bf
AlexanderTheGr <a.dimeas@gmail.com>
2016-12-22 10:38:24 +0200

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -202,8 +202,8 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
 
         $dtparams = array();
         $dtparams[] = array("name" => "ID", "index" => 'id', "active" => "active");
-        $dtparams[] = array("name" => "Κωδικός", "function" => 'geForOrderCode', 'search' => 'text');
-        $dtparams[] = array("name" => "Είδος", "function" => 'geForOrderTitle', 'search' => 'text');
+        $dtparams[] = array("name" => "Κωδικός", "function" => 'getForOrderCode', 'search' => 'text');
+        $dtparams[] = array("name" => "Είδος", "function" => 'getForOrderTitle', 'search' => 'text');
         
         
         $dtparams[] = array("name" => "TecDoc Code", "index" => 'tecdocCode', 'search' => 'text');
