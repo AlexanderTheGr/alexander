583f143e3e2aa6fa2508c07eed3b3c8a8aaa90ae
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-02 15:28:05 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -472,19 +472,19 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         $entities = $this->getDoctrine()
                 ->getRepository('SoftoneBundle:Category')
                 ->findBy(array("parent" => 0));
-        $html = '<ul class="productcategory">';
+        $html = '<ul>';
         
         $cats = $product->getCats();
         
         foreach ($entities as $entity) {
             $html .= "<li>" . $entity->getName();
-            $html .= '<ul class="productcategory">';
+            $html .= '<ul>';
             $entities2 = $this->getDoctrine()
                 ->getRepository('SoftoneBundle:Category')
                 ->findBy(array("parent" => $entity->getId()));            
             foreach ($entities2 as $entity2) {
                 $checked = in_array($entity2->getId(),$cats) ? 'checked' : '';
-                $html .= "<li class='categoryli'><input ".$checked." class='productcategorychk' data-ref='" . $entity2->getId() . "' type='checkbox'/>" . $entity2->getName() . "</li>";
+                $html .= "<li ><input ".$checked." class='productcategory' data-ref='" . $entity2->getId() . "' type='checkbox'/>" . $entity2->getName() . "</li>";
             }
             $html .= '</ul>';
             
