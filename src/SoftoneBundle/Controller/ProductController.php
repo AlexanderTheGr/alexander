d7c3bd3231880acd42b51b25445527e4cab0f567
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-03 12:22:16 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -694,7 +694,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         $datas = $softone->createSql($params);
         foreach ((array) $datas->data as $data) {
             
-            $data = array($data);
+            $data = array($data[0]);
             print_r($data);
 
                 /*
