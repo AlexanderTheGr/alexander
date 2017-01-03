9fa997b590ebdea4b1cd3c18f332080d542b0475
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-03 13:58:35 +0200

    ...

--- src/SoftoneBundle/Controller/SoftoneController.php
+++ src/SoftoneBundle/Controller/SoftoneController.php
@@ -51,7 +51,7 @@ class SoftoneController extends  Main {
         //return;
         foreach ((array)$datas->data as $data) {
             $data = (array) $data;
-            print_r($data);
+            //print_r($data);
             $entity = $this->getDoctrine()
                     ->getRepository($params["repository"])
                     ->findOneBy(array("reference" => (int) $data[$params["softone_table"]]));
@@ -100,8 +100,8 @@ class SoftoneController extends  Main {
                     $entity->$func();
                 }                
             }
-            //if (@$i++ > 500)
-            //    break;
+            if (@$i++ > 200)
+                break;
         }
     }   
 
