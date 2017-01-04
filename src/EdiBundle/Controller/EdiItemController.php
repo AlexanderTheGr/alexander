c5419542b64e4bc9702d1fcce4de1777aa0a6e36
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-04 16:02:42 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -372,11 +372,11 @@ class EdiItemController extends Main {
                 $edi = $dt_columns[1]["search"]["value"];
 
                 //$edi = $em->getRepository("EdiBundle:Edi")->find(1);
-                if ($search[1] ) {
+                //if ($search[1] ) {
                     $this->where = " where " . $this->prefix . ".Edi = '" . $edi . "' AND ((" . $this->prefix . ".tecdocArticleId in (" . (implode(",", $articleIds)) . ") OR " . $this->prefix . ".partno = '" . $search[1] . "' OR " . $this->prefix . ".itemCode = '" . $search[1] . "'))";
-                } else {
-                    $this->where = " where " . $this->prefix . ".Edi = '" . $edi . "' AND (" . $this->prefix . ".tecdocArticleId in (" . (implode(",", $articleIds)) . ")";       
-                }
+                //} else {
+               //     $this->where = " where " . $this->prefix . ".Edi = '" . $edi . "' AND (" . $this->prefix . ".tecdocArticleId in (" . (implode(",", $articleIds)) . ")";       
+                //}
                 
             } else {
                 $this->createWhere();
