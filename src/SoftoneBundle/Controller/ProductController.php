62570f5791f8da2e8eda4b9679da96074773370c
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-04 15:33:23 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -26,12 +26,12 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
      */
     public function indexAction() {
 
-        /*    
-        $products = $this->getDoctrine()->getRepository("SoftoneBundle:Product")
-                ->findAll();
-        foreach ($products as $product) {
-            //$product->setProductFreesearch();
-        }
+        /*
+          $products = $this->getDoctrine()->getRepository("SoftoneBundle:Product")
+          ->findAll();
+          foreach ($products as $product) {
+          //$product->setProductFreesearch();
+          }
          * 
          */
 
@@ -255,6 +255,17 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         return new Response(
                 $json, 200, array('Content-Type' => 'application/json')
         );
+    }
+
+    private function clearCode($code) {
+        $code = str_replace(" ", "", $code);
+        $code = str_replace(".", "", $code);
+        $code = str_replace("-", "", $code);
+        $code = str_replace("/", "", $code);
+        $code = str_replace(")", "", $code);
+        $code = str_replace("(", "", $code);
+        $code = strtoupper($code);
+        return $code;
     }
 
     /**
@@ -715,14 +726,12 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         $this->getDoctrine()->getConnection()->exec($sql);
     }
 
-    
-    
-    function retrieveProduct($params=array()) {
+    function retrieveProduct($params = array()) {
         $object = $params["object"];
         $em = $this->getDoctrine()->getManager();
         $fields = $em->getClassMetadata($params["object"])->getFieldNames();
         //print_r($fields);
-        
+
         $itemfield = array();
         $itemfield[] = "M." . $params["softone_table"];
         foreach ($fields as $field) {
@@ -753,14 +762,14 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         //print_r($datas);
         //return;
         $em = $this->getDoctrine()->getManager();
-        foreach ((array)$datas->data as $data) {
+        foreach ((array) $datas->data as $data) {
             $data = (array) $data;
             //print_r($data);
             $entity = $this->getDoctrine()
                     ->getRepository($params["repository"])
                     ->findOneBy(array("reference" => (int) $data[$params["softone_table"]]));
 
-            echo @$entity->id."<BR>";
+            echo @$entity->id . "<BR>";
             //if ($data[$params["softone_table"]] < 7385) continue;
             $dt = new \DateTime("now");
             if (@$entity->id == 0) {
@@ -768,12 +777,11 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                 $entity->setTs($dt);
                 $entity->setCreated($dt);
                 $entity->setModified($dt);
-                
             } else {
                 continue;
                 //$entity->setRepositories();                
             }
-            
+
             //@print_r($entity->repositories);
             foreach ($params["relation"] as $field => $extra) {
                 //echo $field." - ".@$data[$extra]."<BR>";
@@ -781,23 +789,23 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                     $entity->setField($field, @$data[$extra]);
                 }
                 //echo @$entity->repositories[$field];
-                if (@$data[$extra] AND @$entity->repositories[$field]) {
+                if (@$data[$extra] AND @ $entity->repositories[$field]) {
                     $rel = $this->getDoctrine()->getRepository($entity->repositories[$field])->findOneById($data[$extra]);
                     $entity->setField($field, $rel);
-                }                
+                }
             }
-            echo $data[$params["softone_table"]]."<BR>";
+            echo $data[$params["softone_table"]] . "<BR>";
             /*
-            $imporetedData = array();
-            $entity->setReference($data[$params["softone_table"]]);
-            
-            $em->persist($entity);
-            $em->flush();
-            */
+              $imporetedData = array();
+              $entity->setReference($data[$params["softone_table"]]);
+
+              $em->persist($entity);
+              $em->flush();
+             */
             //$this->flushpersist($entity);
             $q = array();
-            $q[] = "reference = '".$data[$params["softone_table"]]."'";
-            
+            $q[] = "reference = '" . $data[$params["softone_table"]] . "'";
+
             foreach ($data as $identifier => $val) {
                 $imporetedData[strtolower($params["softone_object"] . "_" . $identifier)] = addslashes($val);
                 $ad = strtolower($identifier);
@@ -808,49 +816,47 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                 }
             }
             $sql = "insert " . strtolower($params["table"]) . " set " . implode(",", $q) . "";
-            echo $sql."<BR>";
-            $em->getConnection()->exec($sql);            
+            echo $sql . "<BR>";
+            $em->getConnection()->exec($sql);
             /*
-            @$entity_id = (int) $entity->id;
-            //if (@$entity_id > 0) {
-                $sql = "update " . strtolower($params["table"]) . " set " . implode(",", $q) . " where id = '" . $entity_id . "'";
-                echo $sql."<BR>";
-                $em->getConnection()->exec($sql);
-                foreach ($params["extrafunction"] as $field => $func) {
-                    //$entity->$func();
-                }                
-            //}
+              @$entity_id = (int) $entity->id;
+              //if (@$entity_id > 0) {
+              $sql = "update " . strtolower($params["table"]) . " set " . implode(",", $q) . " where id = '" . $entity_id . "'";
+              echo $sql."<BR>";
+              $em->getConnection()->exec($sql);
+              foreach ($params["extrafunction"] as $field => $func) {
+              //$entity->$func();
+              }
+              //}
              * 
              */
             $entity = null;
             //if (@$i++ > 1500)
             //    break;
         }
-    }     
-    
-   
-    
+    }
+
     /**
      * @Route("/product/product/updatetecdoc")
      */
     public function getUpdateTecdocAction($funct = false) {
         $em = $this->getDoctrine()->getManager();
-        
+
         $query = $em->createQuery(
                 "SELECT  p.id
                     FROM " . $this->repository . " p
                     where p.tecdocSupplierId > 0 AND p.tecdocArticleId IS NULL order by p.id asc"
         );
         /*
-        $query = $em->createQuery(
-                "SELECT  p.id
-                    FROM " . $this->repository . " p, EdiBundle:Edi e
-                    where 
-                        e.id = p.Edi AND p.dlnr > 0 order by p.id asc"
-        );
+          $query = $em->createQuery(
+          "SELECT  p.id
+          FROM " . $this->repository . " p, EdiBundle:Edi e
+          where
+          e.id = p.Edi AND p.dlnr > 0 order by p.id asc"
+          );
          * 
          */
-        
+
         $results = $query->getResult();
         echo count($results);
         $i = 0;
@@ -861,15 +867,13 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                 $ediediitem->tecdoc = $tecdoc;
                 $ediediitem->updatetecdoc();
                 unset($ediediitem);
-                echo $result["id"]."<BR>";
+                echo $result["id"] . "<BR>";
                 //if ($i++ > 3000) exit;
             }
-            
-            
         }
         exit;
-    }    
-    
+    }
+
     /**
      * 
      * @Route("/product/retrieveSoftone")
@@ -880,7 +884,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         //echo $this->retrieveMtrcategory();
         //echo $this->retrieveMtrmanfctr();
         echo $this->retrieveMtrl();
-        
+
 
         return new Response(
                 "", 200
