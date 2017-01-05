9e9aa4566e210f17e948f8edc6f02a41ef8976cf
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-05 15:56:35 +0200

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -2065,7 +2065,7 @@ class Product extends Entity {
             $filters = $object . ".CODE=" . $this->itemCode . "&" . $object . ".CODE_TO=" . $this->itemCode;
             $datas = $softone->retrieveData($object, "partsbox", $filters);
 
-
+            print_r($datas);
 
             foreach ($datas as $data) {
                 $data = (array) $data;
@@ -2106,9 +2106,9 @@ class Product extends Entity {
         
         $dataOut[$object] = (array) $objectArr;
         //@$dataOut["ITEEXTRA"][0] = array("NUM02" => $this->item_mtrl_iteextra_num02);
-        print_r(@$dataOut);
+        //print_r(@$dataOut);
         $out = $softone->setData((array) $dataOut, $object, (int) $this->reference);
-        print_r($out);
+        //print_r($out);
 
         if (@$out->id > 0) {
             $this->reference = $out->id;
