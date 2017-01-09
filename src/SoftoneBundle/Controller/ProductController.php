5eff40ec67dd058e3579f3624c2594482c035f9d
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-09 16:58:22 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -635,6 +635,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
             $fields[] = array("name" => "Title", "index" => 'title');
             $fields[] = array("name" => "Code", "index" => 'erpCode');
             $fields[] = array("name" => "Supplier", "index" => 'supplierId:title');
+            $fields[] = array("name" => "Προσφορά", "index" => 'productSale:title');
             $fields[] = array("name" => "Λιανική", "index" => 'itemPricer');
             $fields[] = array("name" => "Χονδρική", "index" => 'itemPricew');
             $this->setSetting("SoftoneBundle:Product:getdatatable", serialize($fields));
