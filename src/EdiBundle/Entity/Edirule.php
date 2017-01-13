7fadb6fba88ae047456acdbb4db4bfc21fd16c32
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-13 11:39:11 +0200

    ...

--- src/EdiBundle/Entity/Edirule.php
+++ src/EdiBundle/Entity/Edirule.php
@@ -188,6 +188,7 @@ class Edirule {
         if ('AppCache' == get_class($kernel)) {
             $kernel = $kernel->getKernel();
         }
+        $ediitem->tecdoc = new Tecdoc();
         $ediitem->updatetecdoc(true);
         $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
         $cats = $ediitem->getCats();
