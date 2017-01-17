90105aee05ae1cdf55f40aa82245810e94a52ce4
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-17 13:32:38 +0200

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -1458,7 +1458,7 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
             $orderItem->setDisc1prc($request->request->get("discount"));
         elseif ($request->request->get("livevalqty")) {
             //$orderItem->setDisc1prc($request->request->get("discount"));
-            $disc1prc = 1 - ($orderItem->getPrice() / $request->request->get("livevalqty"));
+            $disc1prc = 1 - ($request->request->get("livevalqty") / $orderItem->getPrice());
             $orderItem->setDisc1prc($disc1prc*100);
         } elseif ($request->request->get("qty") == 0) {
             try {
