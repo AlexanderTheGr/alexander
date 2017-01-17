4768ff54c794e8dbb3eafdb1e3827d96091a9a80
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-17 16:21:29 +0200

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -2034,6 +2034,7 @@ class Product extends Entity {
         $fields[] = "item_mtrsup";
         $fields[] = "item_mtrcategory";
         $fields[] = "item_markupw";
+        $fields[] = "item_markupr";
         $fields[] = "item_isactive";
         $fields[] = "item_mtrmark";
         $fields[] = "item_apvcode";
@@ -2083,9 +2084,9 @@ class Product extends Entity {
         $objectArr[0] = $objectArr2;
         $dataOut[$object] = (array) $objectArr;
         //@$dataOut["ITEEXTRA"][0] = array("NUM02" => $this->item_mtrl_iteextra_num02);
-        print_r(@$dataOut);
+       // print_r(@$dataOut);
         $out = $softone->setData((array) $dataOut, $object, (int) $this->reference);
-        print_r($out);
+        //print_r($out);
 
         
         if (@$out->id > 0) {
