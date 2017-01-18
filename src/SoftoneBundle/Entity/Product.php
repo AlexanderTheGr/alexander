60f376d1923d5d1d8333b03b81c437f6e2fe029e
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-18 14:32:32 +0200

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -2084,9 +2084,9 @@ class Product extends Entity {
         $objectArr[0] = $objectArr2;
         $dataOut[$object] = (array) $objectArr;
         //@$dataOut["ITEEXTRA"][0] = array("NUM02" => $this->item_mtrl_iteextra_num02);
-       // print_r(@$dataOut);
+        print_r(@$dataOut);
         $out = $softone->setData((array) $dataOut, $object, (int) $this->reference);
-        //print_r($out);
+        print_r($out);
 
         
         if (@$out->id > 0) {
