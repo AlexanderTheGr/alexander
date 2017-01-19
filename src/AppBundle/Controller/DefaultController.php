617125b7dba39d9e2e4c451457ef2aa7e6eb7c02
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-19 12:59:44 +0200

    ...

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,27 +19,36 @@ class DefaultController extends Controller {
                     'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
         ));
     }
-    
+
     /**
      * @Route("/alerts", name="alerts")
      */
     public function alerts() {
         $alerts = 0;
         $orders = $this->getDoctrine()
-                ->getRepository("SoftoneBundle:Order")->findBy(array("isnew"=>1));
+                        ->getRepository("SoftoneBundle:Order")->findBy(array("isnew" => 1));
         $alerts += count($orders);
-        
-        
+
+
         $ordersHtml = "<ul style='overflow: auto; max-height: 400px;' class='animation-expand'>";
-        foreach($orders as $order) {
-            $ordersHtml .= "<li class='' style='list-style:none'><a href='/order/view/".$order->getId()."'>".$order->getFincode()." ".$order->getCustomerName()."</a></li>";
+        foreach ($orders as $order) {
+            $ordersHtml .= "<li class='' style='list-style:none'><a href='/order/view/" . $order->getId() . "'>" . $order->getFincode() . " " . $order->getCustomerName() . "</a></li>";
         }
         $ordersHtml .= "</ul>";
+
+        $EdiOrders = $this->getDoctrine()
+                        ->getRepository('EdiBundle:EdiOrder')->findBy(array("reference" => 0));
+
+        $ediordersHtml = "<ul style='overflow: auto; max-height: 400px;' class='animation-expand'>";
+        foreach ($ediorders as $ediorder) {
+            $ordersHtml .= "<li class='' style='list-style:none'><a href='/edi/edi/order/view/" . $ediorder->getId() . "'>" . $ediorder->getRemarks() . "</a></li>";
+        }
+        $ediordersHtml .= "</ul>";
         
         return $this->render('default/alerts.html.twig', array(
                     'pagename' => '',
-                    'orderscnt' => "Orders:".count($orders),
-                    'edicnt'    => "EDI: 0",
+                    'orderscnt' => "Orders:" . count($orders),
+                    'edicnt' => "EDI: 0",
                     'orders' => $ordersHtml,
                     'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
         ));
