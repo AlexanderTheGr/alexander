6c38a63a392d7b33b9bf89d5f434c022dca838aa
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-19 10:54:16 +0200

    ...

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,18 +6,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'pagename'=>'',
-            'alerts'=>'',
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+                    'pagename' => '',
+                    'alerts' => '',
+                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
         ));
     }
+
+    public function alerts() {
+        return 'sss';
+    }
+
 }
