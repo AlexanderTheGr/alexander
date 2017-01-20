9608e84787ba68422045c81ef74d0e092f10227b
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-20 17:25:26 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1054,8 +1054,8 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
              * 
              */
             $entity = null;
-            //if (@$i++ > 150)
-            //     break;
+            if (@$i++ > 150)
+                 break;
         }
     }
 
