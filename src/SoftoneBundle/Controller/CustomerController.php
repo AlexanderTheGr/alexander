8dc663106d04671a12819e937c4a0d53d71928ce
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-20 12:09:05 +0200

    ...

--- src/SoftoneBundle/Controller/CustomerController.php
+++ src/SoftoneBundle/Controller/CustomerController.php
@@ -202,12 +202,136 @@ class CustomerController extends \SoftoneBundle\Controller\SoftoneController {
         $params["filter"] = '';
         $params["filter"] = 'WHERE M.SODTYPE=13 ' . $where;
         $params["relation"] = array();
-        $params["extra"] = array("irsdata"=>'irsdata');
+        $params["extra"] = array("irsdata" => 'irsdata');
         $params["extrafunction"] = array();
         $this->setSetting("SoftoneBundle:Customer:retrieveCustomer", serialize($params));
 
         $params = unserialize($this->getSetting("SoftoneBundle:Customer:retrieveCustomer"));
         $this->retrieve($params);
+    }
+
+    function retrieve($params = array()) {
+        $object = $params["object"];
+        $em = $this->getDoctrine()->getManager();
+        $fields = $em->getClassMetadata($params["object"])->getFieldNames();
+        //print_r($fields);
+
+        $itemfield = array();
+        $itemfield[] = "M." . $params["softone_table"];
+        foreach ($fields as $field) {
+            $ffield = " " . $field;
+            if (strpos($ffield, $params["softone_object"]) == true) {
+                $itemfield[] = "M." . strtoupper(str_replace($params["softone_object"], "", $field));
+            }
+        }
+
+        foreach ($params["extra"] as $field => $extra) {
+            //if (@$data[$extra] AND in_array($field, $fields)) {
+            if ($field == $extra)
+                $itemfield[] = "M." . strtoupper($field);
+            else
+                $itemfield[] = "M." . strtoupper($field) . " as $extra";
+            //}
+        }
+
+        $sql = "SELECT * FROM IRSDATA";
+        $params["fSQL"] = $sql;
+        $softone = new Softone();
+        $datas = $softone->createSql($params);
+
+        print_r($datas);
+        foreach ($datas->data as $data) {
+            $IRSDATA[$data->NAME] = $data->IRSDATA;
+
+            
+        }
+        $selfields = implode(",", $itemfield);
+        $params["fSQL"] = 'SELECT ' . $selfields . ' FROM ' . $params["softone_table"] . ' M ' . $params["filter"];
+        //echo $params["fSQL"];
+        //$params["fSQL"] = 'SELECT M.* FROM ' . $params["softone_table"] . ' M ' . $params["filter"];
+        echo "<BR>";
+        echo $params["fSQL"];
+        echo "<BR>";
+
+        $softone = new Softone();
+        $datas = $softone->createSql($params);
+
+        //return;
+        //exit;
+        $em = $this->getDoctrine()->getManager();
+        foreach ((array) $datas->data as $data) {
+            $data = (array) $data;
+            print_r($data);
+            exit;
+            $entity = $this->getDoctrine()
+                    ->getRepository($params["repository"])
+                    ->findOneBy(array("reference" => (int) $data[$params["softone_table"]]));
+
+            echo @$entity->id . "<BR>";
+            //if ($data[$params["softone_table"]] < 7385) continue;
+            $dt = new \DateTime("now");
+            if (@$entity->id == 0) {
+                $entity = new $object();
+                $entity->setTs($dt);
+                $entity->setCreated($dt);
+                $entity->setModified($dt);
+            } else {
+                continue;
+                //$entity->setRepositories();                
+            }
+
+            //@print_r($entity->repositories);
+            foreach ($params["relation"] as $field => $extra) {
+                //echo $field." - ".@$data[$extra]."<BR>";
+                if (@$data[$extra] AND in_array($field, $fields)) {
+                    $entity->setField($field, @$data[$extra]);
+                }
+                //echo @$entity->repositories[$field];
+                if (@$data[$extra] AND @ $entity->repositories[$field]) {
+                    $rel = $this->getDoctrine()->getRepository($entity->repositories[$field])->findOneById($data[$extra]);
+                    $entity->setField($field, $rel);
+                }
+            }
+            echo $data[$params["softone_table"]] . "<BR>";
+            /*
+              $imporetedData = array();
+              $entity->setReference($data[$params["softone_table"]]);
+
+              $em->persist($entity);
+              $em->flush();
+             */
+            //$this->flushpersist($entity);
+            $q = array();
+            $q[] = "reference = '" . $data[$params["softone_table"]] . "'";
+
+            foreach ($data as $identifier => $val) {
+                $imporetedData[strtolower($params["softone_object"] . "_" . $identifier)] = addslashes($val);
+                $ad = strtolower($identifier);
+                $baz = $params["softone_object"] . ucwords(str_replace("_", " ", $ad));
+                if (in_array($baz, $fields)) {
+                    $q[] = "`" . strtolower($params["softone_object"] . "_" . $identifier) . "` = '" . addslashes($val) . "'";
+                    //$entity->setField($baz, $val);
+                }
+            }
+            $sql = "insert " . strtolower($params["table"]) . " set " . implode(",", $q) . "";
+            echo $sql . "<BR>";
+            $em->getConnection()->exec($sql);
+            /*
+              @$entity_id = (int) $entity->id;
+              //if (@$entity_id > 0) {
+              $sql = "update " . strtolower($params["table"]) . " set " . implode(",", $q) . " where id = '" . $entity_id . "'";
+              echo $sql."<BR>";
+              $em->getConnection()->exec($sql);
+              foreach ($params["extrafunction"] as $field => $func) {
+              //$entity->$func();
+              }
+              //}
+             * 
+             */
+            $entity = null;
+            //if (@$i++ > 1500)
+            //    break;
+        }
     }
 
     /**
@@ -217,28 +341,28 @@ class CustomerController extends \SoftoneBundle\Controller\SoftoneController {
 
         $this->retrieveCustomer();
         $em = $this->getDoctrine()->getManager();
-        
+
         /*
-        $sql = "SELECT * FROM IRSDATA";
-        $params["fSQL"] = $sql;
-        $softone = new Softone();
-        $datas = $softone->createSql($params);
-        
-        print_r($datas);
-        foreach($datas->data as $data) {
-            $sql = "Insert softone_customerirs set "
-                    . "id = '".$data->IRSDATA."', "
-                    . "name = '".$data->NAME."', "
-                    . "address = '".$data->ADDRESS."', "
-                    . "district = '".$data->DISTRICT."', "
-                    . "zip = '".$data->ZIP."'"
-                    ;
-            $em->getConnection()->exec($sql);
-        }
+          $sql = "SELECT * FROM IRSDATA";
+          $params["fSQL"] = $sql;
+          $softone = new Softone();
+          $datas = $softone->createSql($params);
+
+          print_r($datas);
+          foreach($datas->data as $data) {
+          $sql = "Insert softone_customerirs set "
+          . "id = '".$data->IRSDATA."', "
+          . "name = '".$data->NAME."', "
+          . "address = '".$data->ADDRESS."', "
+          . "district = '".$data->DISTRICT."', "
+          . "zip = '".$data->ZIP."'"
+          ;
+          $em->getConnection()->exec($sql);
+          }
          * 
          */
         exit;
-        
+
         $params["list"] = 'partsbox';
 
         $params = array("softone_object" => "CUSTOMER", "eav_model" => "customer", "model" => "Customer", "list" => "monitor");
@@ -296,7 +420,7 @@ class CustomerController extends \SoftoneBundle\Controller\SoftoneController {
         }
         $sql = 'update `softone_customer` set `group` = 1 where `group` is null';
         echo $sql;
-        $this->getDoctrine()->getConnection()->exec($sql);              
+        $this->getDoctrine()->getConnection()->exec($sql);
         $datas = $softone->getCustomerAddresses();
         $sql = "truncate softone_customeraddress";
         $em->getConnection()->exec($sql);
@@ -325,7 +449,6 @@ class CustomerController extends \SoftoneBundle\Controller\SoftoneController {
                 $em->getConnection()->exec($sql);
                 //echo $sql . "<BR>";
             }
-
     }
 
     function getRules() {
