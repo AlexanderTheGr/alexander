e2ec9f683d8eb30999cb95d8eeeb11274613c0a8
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-20 11:20:03 +0200

    ...

---
+++ src/SoftoneBundle/Entity/CustomerIrs.php
@@ -0,0 +1,257 @@
+<?php
+
+namespace SoftoneBundle\Entity;
+
+/**
+ * CustomerIrs
+ */
+class CustomerIrs
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $address;
+
+    /**
+     * @var string
+     */
+    private $district;
+
+    /**
+     * @var string
+     */
+    private $zip;
+
+    /**
+     * @var \DateTime
+     */
+    private $ts;
+
+    /**
+     * @var integer
+     */
+    private $actioneer;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     */
+    private $modified;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return CustomerIrs
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return CustomerIrs
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set district
+     *
+     * @param string $district
+     *
+     * @return CustomerIrs
+     */
+    public function setDistrict($district)
+    {
+        $this->district = $district;
+
+        return $this;
+    }
+
+    /**
+     * Get district
+     *
+     * @return string
+     */
+    public function getDistrict()
+    {
+        return $this->district;
+    }
+
+    /**
+     * Set zip
+     *
+     * @param string $zip
+     *
+     * @return CustomerIrs
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    /**
+     * Get zip
+     *
+     * @return string
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * Set ts
+     *
+     * @param \DateTime $ts
+     *
+     * @return CustomerIrs
+     */
+    public function setTs($ts)
+    {
+        $this->ts = $ts;
+
+        return $this;
+    }
+
+    /**
+     * Get ts
+     *
+     * @return \DateTime
+     */
+    public function getTs()
+    {
+        return $this->ts;
+    }
+
+    /**
+     * Set actioneer
+     *
+     * @param integer $actioneer
+     *
+     * @return CustomerIrs
+     */
+    public function setActioneer($actioneer)
+    {
+        $this->actioneer = $actioneer;
+
+        return $this;
+    }
+
+    /**
+     * Get actioneer
+     *
+     * @return integer
+     */
+    public function getActioneer()
+    {
+        return $this->actioneer;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return CustomerIrs
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     *
+     * @return CustomerIrs
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return \DateTime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
