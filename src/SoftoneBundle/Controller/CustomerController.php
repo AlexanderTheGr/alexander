aa88fc1e60a92f228ea7e6fc02eac9b5628d6f7b
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-20 12:50:25 +0200

    ...

--- src/SoftoneBundle/Controller/CustomerController.php
+++ src/SoftoneBundle/Controller/CustomerController.php
@@ -245,14 +245,14 @@ class CustomerController extends \SoftoneBundle\Controller\SoftoneController {
 
         $softone = new Softone();
         $datas = $softone->createSql($params);
-        print_r($datas);
-        //return;
-        exit;
+        //print_r($datas);
+        ///return;
+        ///exit;
         $em = $this->getDoctrine()->getManager();
         foreach ((array) $datas->data as $data) {
             $data = (array) $data;
             //$data["IRSDATA2"] = $IRSDATA[$data["IRSDATA"]];
-            //print_r($data);
+            print_r($data);
             
             if ($i++ > 100 ) exit;
             continue;
