2ec1decdab50a217cd9c45cbca2633b211b55f0e
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-20 13:14:14 +0200

    ...

--- src/SoftoneBundle/Entity/Customer.php
+++ src/SoftoneBundle/Entity/Customer.php
@@ -1022,16 +1022,16 @@ class Customer extends Entity {
         foreach ($fields as $field) {
             $field1 = strtoupper(str_replace(strtolower($object) . "_", "", $field));
             $field2 = lcfirst($this->createName($field));
-            //echo $field2 . "<BR>";
+            echo $field2 . "<BR>";
             @$objectArr2[$field1] = $this->$field2;
             //}
         }
         $objectArr[0] = $objectArr2;
         $dataOut[$object] = (array) $objectArr;
         //@$dataOut["ITEEXTRA"][0] = array("NUM02" => $this->item_mtrl_iteextra_num02);
-        print_r(@$dataOut);
+        //print_r(@$dataOut);
         $out = $softone->setData((array) $dataOut, $object, (int) $this->reference);
-        print_r($out);
+        //print_r($out);
         if (@$out->id > 0) {
             $filters = "CUSTOMER.CODE=" . $this->customerCode . "&CUSTOMER.CODE_TO=" . $this->customerCode;
             $datas = $softone->retrieveData($object, "partsbox", $filters);
