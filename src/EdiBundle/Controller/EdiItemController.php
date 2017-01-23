a6c0e16a3a0b5c77bf5166f3836794faa3d146a6
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-23 13:21:46 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -183,7 +183,13 @@ class EdiItemController extends Main {
                     where 
                         e.id = p.Edi AND p.partno != '' AND
                         (p.partno LIKE '%" . $search[1] . "%' OR p.itemCode LIKE '%" . $search[1] . "%' OR p.tecdocArticleId in (" . implode(",", $articleIds) . ")) "
-            );
+            
+                    );
+            echo "SELECT  distinct(e.id) as eid, e.name as edi
+                    FROM " . $this->repository . " p, EdiBundle:Edi e
+                    where 
+                        e.id = p.Edi AND p.partno != '' AND
+                        (p.partno LIKE '%" . $search[1] . "%' OR p.itemCode LIKE '%" . $search[1] . "%' OR p.tecdocArticleId in (" . implode(",", $articleIds) . "))";
         } else {
             $articleIds = (array) unserialize($this->getArticlesSearch($this->clearstring($search[0])));
            
