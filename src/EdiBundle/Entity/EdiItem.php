6d0c199e382036073736c9b03f6dc230209de48c
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-24 09:44:20 +0200

    ...

--- src/EdiBundle/Entity/EdiItem.php
+++ src/EdiBundle/Entity/EdiItem.php
@@ -780,6 +780,7 @@ class EdiItem extends Entity {
         $product->setErpSupplier($this->brand);
         $product->setItemMtrmanfctr($SoftoneSupplier->getId());
         $product->setErpCode($erpCode);
+        $product->setCccRef($this->itemCode);
         $product->setItemCode($product->getErpCode());
         $product->setItemCode2($this->clearCode($this->partno));
         $product->setEdi($this->getEdi()->getId());
