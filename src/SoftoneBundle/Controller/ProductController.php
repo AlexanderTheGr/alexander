47c495d4a050a50c00b7a179efb18ea890e28d17
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-24 10:56:35 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -610,8 +610,9 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         $fields["title"] = array("label" => "Περιγραφή", "required" => true, "className" => "col-md-6 col-sm-6");
         $fields["erpCode"] = array("label" => "Κωδικός Είδους", "required" => false, "className" => "col-md-3 col-sm-3");
         $fields["itemCode1"] = array("label" => "Barcode", "required" => false, "className" => "col-md-3 col-sm-3");
-        /*
+        
         $fields["tecdocSupplierId"] = array("label" => "Tecdoc Supplier", "required" => false, "className" => "col-md-6", 'type' => "select", 'datasource' => array('repository' => 'SoftoneBundle:TecdocSupplier', 'name' => 'supplier', 'value' => 'id', 'suffix' => 'id'));
+        /*
         $fields["tecdocCode"] = array("label" => "Tecdoc Code", "required" => false, "className" => "col-md-6");
 
         
