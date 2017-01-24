ddf0fbbcca32b7ed0d6f0f61acff1f9e15ab460c
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-24 18:18:58 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -443,7 +443,7 @@ class EdiItemController extends Main {
             
             
             $sql = "Select id from softone_product where replace(replace(replace(replace(replace(`item_cccref`, '/', ''), '.', ''), '-', ''), ' ', ''), '*', '')  = '" . $obj->getItemCode() . "' AND item_mtrsup = '" . $obj->getEdi()->getItemMtrsup() . "'";
-            echo $sql . "<BR>";
+            //echo $sql . "<BR>";
             $connection = $em->getConnection();
             $statement = $connection->prepare($sql);
             $statement->execute();
