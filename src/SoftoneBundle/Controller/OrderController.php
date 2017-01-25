876a1290cca47a5bc7b011de12209da916e23e02
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-25 15:04:01 +0200

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -1315,7 +1315,7 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
             foreach ($table as $f => $val) {
                 if ($f == 0 AND $f != 'DT_RowId' AND $f != 'DT_RowClass') {
                     $table1[$f] = $val;
-                    $table1[1] = $this->getOrderItemsPopup($val);
+                    //$table1[1] = $this->getOrderItemsPopup($val);
                     //$hasOrderItems = $this->getHasOrderItems($val);
                 } else if ($f == 1) {
                     $table1[$f] = $table1[1] . $val;
