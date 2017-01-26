cebef6f91beeb6952b346ca544e109dfda97e921
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-26 13:00:05 +0200

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -602,7 +602,7 @@ class EdiItemController extends Main {
                     $AvailabilityDetailsHtml .= "</select>";        
                     @$jsonarr[$key]['6'] = $entity->getDiscount($customer,$vat);
                     @$jsonarr[$key]['7'] = number_format((float) $xml->Item->Header->PriceOnPolicy, 2, '.', '');
-                    @$jsonarr[$key]['8'] = $jsonarr[$key]['8'].$AvailabilityDetailsHtml;
+                    @$jsonarr[$key]['8'] = $jsonarr[$key]['8'].$AvailabilityDetailsHtml.$xml->Item->Header->SUGGESTED_STORE;
                     @$jsonarr[$key]['DT_RowClass'] .= $xml->Item->Header->Available == "Y" ? ' text-success ' : ' text-danger ';
                 }
             }
