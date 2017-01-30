0d3f2103be765161f9daed13350d7675c3e0a7ad
AlexanderTheGr <a.dimeas@gmail.com>
2017-01-30 15:35:24 +0200

    ...

--- web/assets/js/libs/softone/chat.js
+++ web/assets/js/libs/softone/chat.js
@@ -104,3 +104,75 @@ function sendChat(message, from, to)
 $("#triggeroffcanvaschat").click(function () {
 
 })
+
+
+var url = "/user/chatprocess";
+var from = 'a.dimeas@gmail.com';
+$("#page-wrap").hide();
+$(".togglechat").click(function () {
+    $("#page-wrap").toggle();
+    if ($(this).text() == 'Show Chart') {
+        $(this).text("Hide Chat");
+    } else {
+        $(this).text("Show Chart");
+    }
+})
+// default name is 'Guest'
+if (!from || from === ' ') {
+    from = "Guest";
+}
+$(".chat-left").live("click", function () {
+    $("#user_email").val($(this).attr("from"));
+    ;
+})
+
+// strip tags
+from = from.replace(/(<([^>]+)>)/ig, "");
+
+// display name on page
+$("#name-area").html("You are: <span>" + from + "</span>");
+
+// kick off chat
+var chat = new Chat();
+
+setInterval('chat.update()', 2000);
+
+$(function () {
+
+    chat.getState();
+
+    // watch textarea for key presses
+    $("#sidebarChatMessage").keydown(function (event) {
+
+        var key = event.which;
+        //all keys including return.  
+        if (key >= 33) {
+
+            var maxLength = $(this).attr("maxlength");
+            var length = this.value.length;
+
+            // don't allow new content if length is maxed out
+            if (length >= maxLength) {
+                event.preventDefault();
+            }
+        }
+    });
+    // watch textarea for release of key press
+    $('#sidebarChatMessage').keyup(function (e) {
+        if (e.keyCode == 13) {
+            var to = $("#user_email").val();
+            var text = $(this).val();
+            var maxLength = $(this).attr("maxlength");
+            var length = text.length;
+
+            // send 
+            if (length <= maxLength + 1) {
+
+                chat.send(text, from, to);
+                $(this).val("");
+            } else {
+                $(this).val(text.substring(0, maxLength));
+            }
+        }
+    });
+});
