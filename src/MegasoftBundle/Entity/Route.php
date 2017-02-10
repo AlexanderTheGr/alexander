f60ef5a3fe546a85981ecd283e78255ab46e8915
AlexanderTheGr <a.dimeas@gmail.com>
2017-02-10 11:48:45 +0200

    ...

---
+++ src/MegasoftBundle/Entity/Route.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace MegasoftBundle\Entity;
+
+/**
+ * Route
+ */
+class Route
+{
+    /**
+     * @var string
+     */
+    private $route;
+
+    /**
+     * @var string
+     */
+    private $schedule;
+
+    /**
+     * @var integer
+     */
+    private $status;
+
+    /**
+     * @var integer
+     */
+    private $actioneer;
+
+    /**
+     * @var \DateTime
+     */
+    private $ts;
+
+    /**
+     * @var \DateTime
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     */
+    private $modified;
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $customers;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $orders;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->customers = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->orders = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Set route
+     *
+     * @param string $route
+     *
+     * @return Route
+     */
+    public function setRoute($route)
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    /**
+     * Get route
+     *
+     * @return string
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Set schedule
+     *
+     * @param string $schedule
+     *
+     * @return Route
+     */
+    public function setSchedule($schedule)
+    {
+        $this->schedule = $schedule;
+
+        return $this;
+    }
+
+    /**
+     * Get schedule
+     *
+     * @return string
+     */
+    public function getSchedule()
+    {
+        return $this->schedule;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return Route
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set actioneer
+     *
+     * @param integer $actioneer
+     *
+     * @return Route
+     */
+    public function setActioneer($actioneer)
+    {
+        $this->actioneer = $actioneer;
+
+        return $this;
+    }
+
+    /**
+     * Get actioneer
+     *
+     * @return integer
+     */
+    public function getActioneer()
+    {
+        return $this->actioneer;
+    }
+
+    /**
+     * Set ts
+     *
+     * @param \DateTime $ts
+     *
+     * @return Route
+     */
+    public function setTs($ts)
+    {
+        $this->ts = $ts;
+
+        return $this;
+    }
+
+    /**
+     * Get ts
+     *
+     * @return \DateTime
+     */
+    public function getTs()
+    {
+        return $this->ts;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Route
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set modified
+     *
+     * @param \DateTime $modified
+     *
+     * @return Route
+     */
+    public function setModified($modified)
+    {
+        $this->modified = $modified;
+
+        return $this;
+    }
+
+    /**
+     * Get modified
+     *
+     * @return \DateTime
+     */
+    public function getModified()
+    {
+        return $this->modified;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add customer
+     *
+     * @param \MegasoftBundle\Entity\Customer $customer
+     *
+     * @return Route
+     */
+    public function addCustomer(\MegasoftBundle\Entity\Customer $customer)
+    {
+        $this->customers[] = $customer;
+
+        return $this;
+    }
+
+    /**
+     * Remove customer
+     *
+     * @param \MegasoftBundle\Entity\Customer $customer
+     */
+    public function removeCustomer(\MegasoftBundle\Entity\Customer $customer)
+    {
+        $this->customers->removeElement($customer);
+    }
+
+    /**
+     * Get customers
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCustomers()
+    {
+        return $this->customers;
+    }
+
+    /**
+     * Add order
+     *
+     * @param \MegasoftBundle\Entity\Order $order
+     *
+     * @return Route
+     */
+    public function addOrder(\MegasoftBundle\Entity\Order $order)
+    {
+        $this->orders[] = $order;
+
+        return $this;
+    }
+
+    /**
+     * Remove order
+     *
+     * @param \MegasoftBundle\Entity\Order $order
+     */
+    public function removeOrder(\MegasoftBundle\Entity\Order $order)
+    {
+        $this->orders->removeElement($order);
+    }
+
+    /**
+     * Get orders
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getOrders()
+    {
+        return $this->orders;
+    }
+}
+
--- src/MegasoftBundle/Entity/Vat.php
+++ src/MegasoftBundle/Entity/Vat.php
@@ -110,4 +110,3 @@ class Vat
         return $this->id;
     }
 }
-
