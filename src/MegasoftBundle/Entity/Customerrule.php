7e2d15885a50d924cac3fdf1241dc8285ea54497
AlexanderTheGr <a.dimeas@gmail.com>
2017-02-14 11:48:11 +0200

    ...

--- src/MegasoftBundle/Entity/Customerrule.php
+++ src/MegasoftBundle/Entity/Customerrule.php
@@ -182,5 +182,181 @@ class Customerrule {
     public function getCustomer() {
         return $this->customer;
     }
+    
+    function validateRule($product, $editem = false) {
+        global $kernel;
+        if ('AppCache' == get_class($kernel)) {
+            $kernel = $kernel->getKernel();
+        }
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
 
+        if ($editem) {
+            $cats = $editem->getCats();
+        } else {
+            $cats = $product->getCats();
+        }
+
+        $rule = json_decode($this->rule, true);
+        $categories = $em->getRepository("MegasoftBundle:Category")->findById($cats);
+        $categoriesArr = array();
+        $catsEp = array();
+        //print_r($cats);
+        foreach ($categories as $category) {
+            $catsEp[] = $category->getSortCode();
+            $pcategory = $em->getRepository("MegasoftBundle:Category")->find($category->getParent());
+            $catsEp[] = $pcategory->getSortCode();
+        }
+        //print_r($catsEp);
+        $supplier = 122220;
+        $productsale = 1;
+        $erpcode = '';
+        if ($editem) {
+            $MegasoftSupplier = $em->getRepository("MegasoftBundle:MegasoftSupplier")
+                    ->findOneBy(array('title' => $editem->getBrand()));
+            if ($MegasoftSupplier)
+                $supplier = $MegasoftSupplier->getId();
+        } else {
+            $supplier = $product->getSupplierId() ? $product->getSupplierId()->getId() : 0;
+            if ($product->getProductsale()) {
+                $productsale = $product->getProductsale()->getId();
+            }
+            $erpcode = $product->getErpCode();
+        }
+        //
+        //echo $this->rulesLoop($rule, $catsEp, $supplier) ? "true" : "false";
+        return $this->rulesLoop($rule, $catsEp, $supplier, $erpcode, $productsale);
+    }
+
+    function rulesLoop($rule, $catsEp, $supplier, $code, $productsale) {
+        foreach ($rule["rules"] as $rl) {
+
+            if (count($rl["rules"])) {
+                $out = $this->rulesLoop($rl, $catsEp, $supplier, $code);
+                if ($rule["condition"] == "OR" AND $out == true) {
+                    return true;
+                }
+                if ($rule["condition"] == "AND" AND $out == false) {
+                    return false;
+                }
+            }
+            if ($rule["condition"] == "OR") {
+                $out = false;
+                if ($rl["id"] == "default") {
+                    return true;
+                }
+                if ($rl["id"] == "category") {
+                    if ($rl["operator"] == "equal") {
+                        if (in_array($rl["value"], $catsEp)) {
+                            return true;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if (!in_array($rl["value"], $catsEp)) {
+                            return true;
+                        }
+                    }
+                }
+                if ($rl["id"] == "supplier") {
+                    if ($rl["operator"] == "equal") {
+                        if ($rl["value"] == $supplier) {
+                            return true;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if ($rl["value"] != $supplier) {
+                            return true;
+                        }
+                    }
+                }
+
+                if ($rl["id"] == "productsale") {
+                    if ($rl["operator"] == "equal") {
+                        if ($rl["value"] == $productsale) {
+                            return true;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if ($rl["value"] != $productsale) {
+                            return true;
+                        }
+                    }
+                }
+
+
+                if ($rl["id"] == "code") {
+                    if ($rl["operator"] == "equal") {
+                        if ($rl["value"] == $code) {
+                            return true;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if ($rl["value"] != $code) {
+                            return true;
+                        }
+                    }
+                }
+            } elseif ($rule["condition"] == "AND") {
+                $out = true;
+
+                if ($rl["id"] == "default") {
+                    return true;
+                }
+                if ($rl["id"] == "category") {
+                    if ($rl["operator"] == "equal") {
+                        if (!in_array($rl["value"], $catsEp)) {
+                            return false;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if (in_array($rl["value"], $catsEp)) {
+                            return false;
+                        }
+                    }
+                }
+
+                if ($rl["id"] == "supplier") {
+                    if ($rl["operator"] == "equal") {
+                        if ($rl["value"] != $supplier) {
+                            return false;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if ($rl["value"] == $supplier) {
+                            return false;
+                        }
+                    }
+                }
+
+
+                if ($rl["id"] == "productsale") {
+                    if ($rl["operator"] == "equal") {
+                        if ($rl["value"] != $productsale) {
+                            return false;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if ($rl["value"] == $productsale) {
+                            return false;
+                        }
+                    }
+                }
+
+
+
+                if ($rl["id"] == "code") {
+                    if ($rl["operator"] == "equal") {
+                        if ($rl["value"] != $code) {
+                            return false;
+                        }
+                    }
+                    if ($rl["operator"] == "not_equal") {
+                        if ($rl["value"] == $code) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return $out;
+    }
 }
