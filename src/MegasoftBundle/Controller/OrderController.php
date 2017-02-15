6bbdbe2393fca37fce17dc08a08d9261cff0f46e
AlexanderTheGr <a.dimeas@gmail.com>
2017-02-15 14:11:59 +0200

    ...

--- src/MegasoftBundle/Controller/OrderController.php
+++ src/MegasoftBundle/Controller/OrderController.php
@@ -312,7 +312,8 @@ class OrderController extends Main {
         $dtparams[] = array("name" => "Είδος", "index" => 'product:title');
         $dtparams[] = array("name" => "Κωδικός Είδους", "index" => 'product:erpCode');
         $dtparams[] = array("name" => "Ράφι", "index" => 'product:itemMtrplace');
-        $dtparams[] = array("name" => "Supplier", "index" => 'product:supplierId:title');
+        //$dtparams[] = array("name" => "Supplier", "index" => 'product:supplierId:title');
+        $dtparams[] = array("name" => "Supplier", "index" => 'erpSupplier');
         $dtparams[] = array("name" => "Αποθηκη", "function" => 'getProductApothiki', 'search' => 'text');
         $dtparams[] = array("name" => "Qty", "input" => "text", "index" => 'qty');
         $dtparams[] = array("name" => "Τιμή Καταλόγου", "input" => "text", "index" => 'price');
