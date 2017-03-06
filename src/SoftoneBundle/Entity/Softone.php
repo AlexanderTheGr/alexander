b3efded883e56831fbf5c4e571a28558c96dc5bb
AlexanderTheGr <a.dimeas@gmail.com>
2017-03-06 16:43:36 +0200

    ...

--- src/SoftoneBundle/Entity/Softone.php
+++ src/SoftoneBundle/Entity/Softone.php
@@ -327,7 +327,7 @@ class Softone extends Entity {
 
         if (@$result1 = gzdecode($result)) {
             
-            echo $result;
+            echo $result1;
             
             $result = iconv("ISO-8859-7", "UTF-8", $result1);
         } else {
