3c66f3c68e0c25aaec670928645f210bee81bf38
AlexanderTheGr <a.dimeas@gmail.com>
2017-05-04 11:26:46 +0300

    ...

---
+++ src/MegasoftBundle/Controller/SupplierController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace MegasoftBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Controller\Main as Main;
+
+class SupplierController extends Main {
+
+    var $repository = 'MegasoftBundle:Supplier';
+
+    /**
+     * @Route("/megasoft/supplier/supplier")
+     */
+    public function indexAction() {
+
+        return $this->render('supplier/index.html.twig', array(
+                    'pagename' => 'Προμηθευτές',
+                    'url' => '/megasoft/supplier/getdatatable',
+                    'view' => '/supplier/view',
+                    'ctrl' => $this->generateRandomString(),
+                    'app' => $this->generateRandomString(),
+                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
+        ));
+    }
+
+    /**
+     * @Route("/megasoft/supplier/view/{id}")
+     */
+    public function viewAction($id) {
+
+        return $this->render('supplier/view.html.twig', array(
+                    'pagename' => 'Supplier',
+                    'url' => '/supplier/save',
+                    'ctrl' => $this->generateRandomString(),
+                    'app' => $this->generateRandomString(),
+                    'tabs' => $this->gettabs($id),
+                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
+        ));
+    }
+
+    /**
+     * @Route("/megasoft/supplier/save")
+     */
+    public function savection() {
+        $this->save();
+        $json = json_encode(array("ok"));
+        return new Response(
+                $json, 200, array('Content-Type' => 'application/json')
+        );
+    }
+
+    /**
+     * @Route("/megasoft/supplier/gettab")
+     */
+    public function gettabs($id) {
+
+
+        $entity = $this->getDoctrine()
+                ->getRepository($this->repository)
+                ->find($id);
+
+        $fields["supplierCode"] = array("label" => "Code");
+        $fields["supplierName"] = array("label" => "Name");
+
+        $forms = $this->getFormLyFields($entity, $fields);
+
+        $this->addTab(array("title" => "General", "form" => $forms, "content" => '', "index" => $this->generateRandomString(), 'search' => 'text', "active" => true));
+        $json = $this->tabs();
+        return $json;
+    }
+
+    /**
+     * @Route("/megasoft/supplier/getdatatable")
+     */
+    public function getdatatableAction(Request $request) {
+        $this->repository = 'MegasoftBundle:Supplier';
+
+        $this->addField(array("name" => "ID", "index" => 'id', "active" => "active"))
+                ->addField(array("name" => "Code", "index" => 'supplierCode'))
+                ->addField(array("name" => "Name", "index" => 'supplierName'));
+        $json = $this->datatable();
+        return new Response(
+                $json, 200, array('Content-Type' => 'application/json')
+        );
+    }
+    /**
+     * @Route("/megasoft/supplier/retrieve")
+     */    
+    function retrieveSupplier() {
+        $where = '';
+        $params["softone_object"] = 'supplier';
+        $params["repository"] = 'MegasoftBundle:Supplier';
+        $params["softone_table"] = 'TRDR';
+        $params["table"] = 'softone_supplier';
+        $params["object"] = 'MegasoftBundle\Entity\Supplier';
+        $params["filter"] = '';
+        $params["filter"] = 'WHERE M.SODTYPE=12 ' . $where;
+        $params["relation"] = array();
+        $params["extra"] = array();
+        $params["extrafunction"] = array();
+        $this->setSetting("MegasoftBundle:Supplier:retrieveSupplier", serialize($params));
+
+        $params = unserialize($this->getSetting("MegasoftBundle:Supplier:retrieveSupplier"));
+        $this->retrieve($params);
+        return new Response(
+                json_encode(array()), 200, array('Content-Type' => 'application/json')
+        );        
+    }
+}
