6c272e33f62411705e96b39b60b08c4b0d984a7b
AlexanderTheGr <a.dimeas@gmail.com>
2017-05-25 12:58:01 +0300

    ...

--- src/MegasoftBundle/Entity/Customergrouprule.php
+++ src/MegasoftBundle/Entity/Customergrouprule.php
@@ -181,8 +181,8 @@ class Customergrouprule {
             if ($Manufacturer)
                 $supplier = $Manufacturer->getId();
         } else {
-            //$supplier = $product->getSupplier() ? $product->getSupplier()->getId() : 0;
-            $supplier = 0;
+            $supplier = $product->getSupplier() ? $product->getSupplier()->getId() : 0;
+            //$supplier = 0;
             if ($product->getProductsale()) {
                 $productsale = $product->getProductsale()->getId();
             }
