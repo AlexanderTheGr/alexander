1412617a732068fa396238adc3ad6930c20514b3
AlexanderTheGr <a.dimeas@gmail.com>
2017-05-25 13:30:53 +0300

    ...

--- src/MegasoftBundle/Entity/Customer.php
+++ src/MegasoftBundle/Entity/Customer.php
@@ -816,7 +816,7 @@ class Customer extends Entity {
         $data["CustomerPhone2"] = $this->customerPhone2;
         $data["CustomerDoy"] = $this->customerIrsdata;
         $data["CustomerOccupation"] = $this->customerJobtypetrd;
-        $data["CustomerPricelist"] = $this->pricelist == 'storeRetailPrice' ? 2 : 1; //$this->CustomerPricelist;
+        $data["CustomerPricelist"] = $this->pricelist == 'store_retail_price' ? 2 : 1; //$this->CustomerPricelist;
         
         /*
           $ns = 'http://schemas.xmlsoap.org/soap/envelope/';
