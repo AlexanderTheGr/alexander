35ab034dd3ed00fe13eccb7cd303b0af12efff41
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-08 16:57:37 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1086,7 +1086,9 @@ class ProductController extends Main {
         $opts = array('http' =>
             array(
                 'method'  => 'POST',
-                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'header'  => 'Content-Type: application/x-www-form-urlencoded\n\r'
+                . 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405',
+                
                 'content' => $postdata
             )
         );
