e18b92e53460d13723309065e36d120295ab5ce2
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-08 17:02:21 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1089,7 +1089,7 @@ class ProductController extends Main {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $server_output = curl_exec($ch);
-
+        echo  $server_output;
         curl_close($ch);
 
 
