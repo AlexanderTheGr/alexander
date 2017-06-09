44ba9d92356fbecbba3d977f82a6ae40eb0127f6
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-09 10:46:41 +0300

    ...

--- src/EdiBundle/Entity/EdiItem.php
+++ src/EdiBundle/Entity/EdiItem.php
@@ -1515,7 +1515,7 @@ class EdiItem extends Entity {
         $discountedPrice = $this->getEdiMarkupPrice($pricefield) * (1 - $discount / 100 );
         $finalprice = $discount > 0 ? $discountedPrice : $price;
 
-        return number_format($finalprice * $vat, 2, '.', '') . " (" . (float) $discount . "%) ".$pricefield;
+        return number_format($finalprice * $vat, 2, '.', '') . " (" . (float) $discount . "%)";
     }
 
 }
