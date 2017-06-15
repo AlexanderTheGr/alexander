f6c03d8a6281c69cc14e7312c94f3ec1bc0063f5
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-15 12:37:00 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1156,6 +1156,7 @@ class ProductController extends Main {
         foreach ($StoreDetails as $data) {
             $this->setProduct($data);
             //exit;
+            if ($i++ > 100) exit;
         }
         $sql = 'UPDATE  `megasoft_product` SET tecdoc_supplier_id = NULL WHERE  `tecdoc_supplier_id` = 0';
         $this->getDoctrine()->getConnection()->exec($sql);
