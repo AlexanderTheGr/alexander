6e8a5c15a1aea4222f110a8f62ce712ebb4c898e
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-21 16:41:41 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1253,12 +1253,12 @@ class ProductController extends Main {
             //$q[] = "`reference` = '" . addslashes($data["StoreId"]) . "'";
 
             $sql = "insert " . strtolower($params["table"]) . " set " . implode(",", $q) . "";
-            echo $sql . "<BR>";
-            echo ".";
+            //echo $sql . "<BR>";
+            echo "-";
             $this->getDoctrine()->getManager()->getConnection()->exec($sql);
         } else {
             $sql = "update " . strtolower($params["table"]) . " set " . implode(",", $q) . " where id = '" . $entity->getId() . "'";
-            echo $sql . "<BR>";
+            //echo $sql . "<BR>";
             echo ".";
             $this->getDoctrine()->getManager()->getConnection()->exec($sql);
         }
