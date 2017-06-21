ec7f80031316be3f4343c3a84d703a34eefec23f
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-21 16:35:11 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1132,7 +1132,7 @@ class ProductController extends Main {
         //echo count($xml);
         //$params["Date"] = "2016-06-21";
         //$response = $soap->__soapCall("GetProducts", array($params));
-        echo count($response->DownloadStoreBaseResponse);
+        echo count($response->DownloadStoreBaseResponse->StoreDetails);
         exit;
 
 
