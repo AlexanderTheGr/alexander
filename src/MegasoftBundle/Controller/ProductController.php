9ad452c11663cd5a46946c65670a21ae7f719192
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-22 09:25:23 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1260,7 +1260,7 @@ class ProductController extends Main {
             $sql = "update " . strtolower($params["table"]) . " set " . implode(",", $q) . " where id = '" . $entity->getId() . "'";
             //echo $sql . "<BR>";
             echo ".";
-            $this->getDoctrine()->getManager()->getConnection()->exec($sql);
+            //$this->getDoctrine()->getManager()->getConnection()->exec($sql);
         }
         $entity = $this->getDoctrine()
                 ->getRepository($this->repository)
