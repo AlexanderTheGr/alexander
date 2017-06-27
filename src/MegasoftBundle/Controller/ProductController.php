1b0743c80b25d0e0fecb59c07b25321409e59e58
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-27 14:08:33 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -767,7 +767,7 @@ class ProductController extends Main {
                 continue;
             $html .= "<li class='brandli' data-ref='" . $brand->getId() . "'>";
             $html .= "<a " . $style . " data-ref='" . $brand->getId() . "' class='brandlia'>" . $brand->getBrand() . "</a>";
-            $html .= "<ul class='pbrandmodels'>";
+            $html .= "<ul  style='display:none' class='pbrandmodels'>";
             foreach ($brandmodels as $brandmodel) {
                 /*
                   $brandmodeltypes = $this->getDoctrine()
