b2fc4e802c07cf742c6ead244c293cf0bc1d810f
AlexanderTheGr <a.dimeas@gmail.com>
2017-06-30 15:10:19 +0300

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1369,7 +1369,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                     //if (addslashes($data["CCCFXRELTDCODE"]) != '')
                         $q[] = "`" . strtolower($params["softone_object"] . "_apvcode") . "` = '" . addslashes($data["CCCFXRELTDCODE"]) . "'";
 
-                    if (addslashes($data["CCCFXRELBRAND"]) != '')
+                    if (addslashes($data["CCCFXRELBRAND"]) != '' AND $hasmark == false)
                         $q[] = "`" . strtolower($params["softone_object"] . "_mtrmark") . "` = '" . addslashes($data["CCCFXRELBRAND"]) . "'";
                 }
             } else {
