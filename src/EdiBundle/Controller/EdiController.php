fd45403014e2ecd70c82fd3a4f68aec1b6de7c12
AlexanderTheGr <a.dimeas@gmail.com>
2017-07-07 11:10:58 +0300

    ...

--- src/EdiBundle/Controller/EdiController.php
+++ src/EdiBundle/Controller/EdiController.php
@@ -710,6 +710,7 @@ class EdiController extends Main {
                 if ($attributes['artnr'] == '') {
                     $attributes['artnr'] = $attributes['similarartnr'];
                 }
+                
                 echo ".";
                 if ($attributes['similarartnr'] != '')  continue;
 
