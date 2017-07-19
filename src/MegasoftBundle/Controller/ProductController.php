e2c4a42ac3025cb384c6900b1f171ee835f3624d
AlexanderTheGr <a.dimeas@gmail.com>
2017-07-19 14:49:05 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1305,7 +1305,10 @@ class ProductController extends Main {
         //return;
         //if ($data["StoreKwd"] != "1643070G") return;
         
-        if ((int)$data["SupplierId"] == 0) return;
+        if ((int)$data["SupplierId"] == 0) 
+            
+        echo (int)$data["SupplierId"]."<BR>";    
+            return;
         $entity = $this->getDoctrine()
                 ->getRepository($this->repository)
                 ->findOneBy(array("erpCode" => $data["StoreKwd"]));
