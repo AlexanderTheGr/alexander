8e92a47084761f9609c9627691b399b628971b36
AlexanderTheGr <a.dimeas@gmail.com>
2017-09-01 14:33:56 +0300

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1116,7 +1116,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
 
                 //$where = " AND UPDDATE = '" . date("Y-m-d") . "'";
             } else {
-                $where = " AND UPDDATE >= '" . date("Y-m-d", strtotime("-200 days")) . "' ORDER BY MTRL";
+                $where = " AND UPDDATE >= '" . date("Y-m-d", strtotime("-2 days")) . "' ORDER BY MTRL";
             }
 
             if ($MTRL > 0) {
@@ -1432,8 +1432,8 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
              * 
              */
             $entity = null;
-            //if (@$i++ > 150)
-            //    break;
+            if (@$i++ > 150)
+                break;
         }
     }
 
