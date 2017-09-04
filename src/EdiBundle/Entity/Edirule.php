cfb2ddd2f4789affece7c7f5b77cc4202dde0d92
AlexanderTheGr <a.dimeas@gmail.com>
2017-09-04 16:57:18 +0300

    ...

--- src/EdiBundle/Entity/Edirule.php
+++ src/EdiBundle/Entity/Edirule.php
@@ -183,10 +183,9 @@ class Edirule {
         return $this->group;
     }
 
-	
     function validateRule($ediitem) {
-		
-		
+
+
         global $kernel;
         if ('AppCache' == get_class($kernel)) {
             $kernel = $kernel->getKernel();
@@ -208,14 +207,18 @@ class Edirule {
             $catsEp[] = $pcategory->getSortCode();
         }
         //echo $ediitem->getBrand();
-        $SoftoneSupplier = $em->getRepository("SoftoneBundle:SoftoneSupplier")
-                ->findOneBy(array('title' => $this->fix($ediitem->getBrand())));
-        
+        if ($this->getSetting("AppBundle:Erp:erpprefix") == '/erp01') {
+            $SoftoneSupplier = $em->getRepository("MegasoftBundle:Manufacturer")
+                    ->findOneBy(array('title' => $this->fix($ediitem->getBrand())));
+        } else {
+            $SoftoneSupplier = $em->getRepository("SoftoneBundle:SoftoneSupplier")
+                    ->findOneBy(array('title' => $this->fix($ediitem->getBrand())));
+        }
         //print_r($rule);
-        
+
         if ($SoftoneSupplier)
             $supplier = $SoftoneSupplier->getId();
-       
+
         //$supplier = $ediitem->getSupplierId()->getId();
         //echo $this->rulesLoop($rule, $catsEp, $supplier) ? "true" : "false";
         return $this->rulesLoop($rule, $catsEp, $supplier, $ediitem->getItemCode());
@@ -237,7 +240,7 @@ class Edirule {
                 $out = false;
                 if ($rl["id"] == "default") {
                     return true;
-                }                
+                }
                 if ($rl["id"] == "category") {
                     if ($rl["operator"] == "equal") {
                         if (in_array($rl["value"], $catsEp)) {
@@ -319,9 +322,8 @@ class Edirule {
             }
         }
         return $out;
-    }    
-	
-	
+    }
+
     function fix2($supplier) {
         $supplier = str_replace("MANN", "MANN-FILTER", $supplier);
         $supplier = str_replace("MEAT&DORIA", "MEAT & DORIA", $supplier);
@@ -356,53 +358,89 @@ class Edirule {
         $supplier = str_replace("LEMF?RDER", "LEMFORDER", $supplier);
         $supplier = str_replace("SALERI", "Saleri SIL", $supplier);
         $supplier = str_replace("CASTROL LUBRICANTS", "CASTROL", $supplier);
-        
-        return $supplier;
-    } 
 
-    function fix($supplier) {
-		if ($supplier == "FEBI") $supplier = str_replace("FEBI", "FEBI BILSTEIN", $supplier);
-        if ($supplier == "MANN") $supplier = str_replace("MANN", "MANN-FILTER", $supplier);
-		if ($supplier == "MEAT&DORIA")  $supplier = str_replace("MEAT&DORIA", "MEAT & DORIA", $supplier);
-        if ($supplier == "BEHR-HELLA") $supplier = str_replace("BEHR-HELLA", "BEHR HELLA SERVICE", $supplier);
-        if ($supplier == "BLUEPRINT") $supplier = str_replace("BLUEPRINT", "BLUE-PRINT", $supplier);
-        if ($supplier == "BLUE PRINT") $supplier = str_replace("BLUE PRINT", "BLUE-PRINT", $supplier);
-        if ($supplier == "BENDIX WBK") $supplier = str_replace("BENDIX WBK", "BENDIX", $supplier);
-        if ($supplier == "CONTI-TECH") $supplier = str_replace("CONTI-TECH", "CONTITECH", $supplier);
-        if ($supplier == "Fai AutoParts") $supplier = str_replace("Fai AutoParts", "FAI AutoParts", $supplier);
-        if ($supplier == "FIAAM") $supplier = str_replace("FIAAM", "COOPERSFIAAM FILTERS", $supplier);
-        if ($supplier == "FIBA") $supplier = str_replace("FIBA", "FI.BA", $supplier);
-        if ($supplier == "FLENOR") $supplier = str_replace("FLENOR", "FLENNOR", $supplier);
-        if ($supplier == "FRITECH") $supplier = str_replace("FRITECH", "fri.tech.", $supplier);
-        if ($supplier == "HERTH & BUSS JAKOPARTS") $supplier = str_replace("HERTH & BUSS JAKOPARTS", "HERTH+BUSS JAKOPARTS", $supplier);
-        if ($supplier == "KAYABA") $supplier = str_replace("KAYABA", "KYB", $supplier);
-        if ($supplier == "KM") $supplier = str_replace("KM", "KM Germany", $supplier);
-        if ($supplier == "LUK") $supplier = str_replace("LUK", "LuK", $supplier);
-        if ($supplier == "FEBI BILSTEIN BILSTEIN") $supplier = str_replace("FEBI BILSTEIN BILSTEIN", "FEBI BILSTEIN", $supplier);
-        if ($supplier == "COOPERSCOOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS FILTERS") $supplier = str_replace("COOPERSCOOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS FILTERS", "COOPERSFIAAM FILTERS", $supplier);
-        if ($supplier == "COOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS") $supplier = str_replace("COOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS", "COOPERSFIAAM FILTERS", $supplier);
-        if ($supplier == "COOPERSCOOPERSFIAAM FILTERS FILTERS") $supplier = str_replace("COOPERSCOOPERSFIAAM FILTERS FILTERS", "COOPERSFIAAM FILTERS", $supplier);
-        if ($supplier == "CoopersFiaam") $supplier = str_replace("CoopersFiaam", "COOPERSFIAAM FILTERS", $supplier);
-        if ($supplier == "MANN") $supplier = str_replace("MANN", "MANN-FILTER", $supplier);
-        if ($supplier == "MANN-FILTER-FILTER-FILTER-FILTER") $supplier = str_replace("MANN-FILTER-FILTER-FILTER-FILTER", "MANN-FILTER", $supplier);
-        if ($supplier == "MANN-FILTER-FILTER") $supplier = str_replace("MANN-FILTER-FILTER", "MANN-FILTER", $supplier);
-        if ($supplier == "MANN-FILTER-FILTER") $supplier = str_replace("MANN-FILTER-FILTER", "MANN-FILTER", $supplier);
-        if ($supplier == "MANN-FILTEREX") $supplier = str_replace("MANN-FILTEREX", "MANN-FILTER", $supplier);
-        if ($supplier == "METALCAUCHO") $supplier = str_replace("METALCAUCHO", "Metalcaucho", $supplier);
-        if ($supplier == "MULLER") $supplier = str_replace("MULLER", "MULLER FILTER", $supplier);
-        if ($supplier == "RICAMBI") $supplier = str_replace("RICAMBI", "GENERAL RICAMBI", $supplier);
-        if ($supplier == "ZIMMERMANN-FILTER") $supplier = str_replace("VERNET", "CALORSTAT by Vernet", $supplier);
-        if ($supplier == "ZIMMERMANN-FILTER") $supplier = str_replace("ZIMMERMANN-FILTER", "ZIMMERMANN", $supplier);
-        if ($supplier == "LESJ?FORS") $supplier = str_replace("LESJ?FORS", "LESJOFORS", $supplier);
-        if ($supplier == "LEMF?RDER") $supplier = str_replace("LEMF?RDER", "LEMFORDER", $supplier);
-        if ($supplier == "SALERI") $supplier = str_replace("SALERI", "Saleri SIL", $supplier);
-        if ($supplier == "CASTROL LUBRICANTS") $supplier = str_replace("CASTROL LUBRICANTS", "CASTROL", $supplier);
-		if ($supplier == "FEBI BILSTEIN BILSTEIN") $supplier = str_replace("FEBI BILSTEIN BILSTEIN", "FEBI BILSTEIN", $supplier);
-		if ($supplier == "KM Germany Germany") $supplier = str_replace("KM Germany Germany", "KM Germany", $supplier);
         return $supplier;
     }
-	
-	
+
+    function fix($supplier) {
+        if ($supplier == "FEBI")
+            $supplier = str_replace("FEBI", "FEBI BILSTEIN", $supplier);
+        if ($supplier == "MANN")
+            $supplier = str_replace("MANN", "MANN-FILTER", $supplier);
+        if ($supplier == "MEAT&DORIA")
+            $supplier = str_replace("MEAT&DORIA", "MEAT & DORIA", $supplier);
+        if ($supplier == "BEHR-HELLA")
+            $supplier = str_replace("BEHR-HELLA", "BEHR HELLA SERVICE", $supplier);
+        if ($supplier == "BLUEPRINT")
+            $supplier = str_replace("BLUEPRINT", "BLUE-PRINT", $supplier);
+        if ($supplier == "BLUE PRINT")
+            $supplier = str_replace("BLUE PRINT", "BLUE-PRINT", $supplier);
+        if ($supplier == "BENDIX WBK")
+            $supplier = str_replace("BENDIX WBK", "BENDIX", $supplier);
+        if ($supplier == "CONTI-TECH")
+            $supplier = str_replace("CONTI-TECH", "CONTITECH", $supplier);
+        if ($supplier == "Fai AutoParts")
+            $supplier = str_replace("Fai AutoParts", "FAI AutoParts", $supplier);
+        if ($supplier == "FIAAM")
+            $supplier = str_replace("FIAAM", "COOPERSFIAAM FILTERS", $supplier);
+        if ($supplier == "FIBA")
+            $supplier = str_replace("FIBA", "FI.BA", $supplier);
+        if ($supplier == "FLENOR")
+            $supplier = str_replace("FLENOR", "FLENNOR", $supplier);
+        if ($supplier == "FRITECH")
+            $supplier = str_replace("FRITECH", "fri.tech.", $supplier);
+        if ($supplier == "HERTH & BUSS JAKOPARTS")
+            $supplier = str_replace("HERTH & BUSS JAKOPARTS", "HERTH+BUSS JAKOPARTS", $supplier);
+        if ($supplier == "KAYABA")
+            $supplier = str_replace("KAYABA", "KYB", $supplier);
+        if ($supplier == "KM")
+            $supplier = str_replace("KM", "KM Germany", $supplier);
+        if ($supplier == "LUK")
+            $supplier = str_replace("LUK", "LuK", $supplier);
+        if ($supplier == "FEBI BILSTEIN BILSTEIN")
+            $supplier = str_replace("FEBI BILSTEIN BILSTEIN", "FEBI BILSTEIN", $supplier);
+        if ($supplier == "COOPERSCOOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS FILTERS")
+            $supplier = str_replace("COOPERSCOOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS FILTERS", "COOPERSFIAAM FILTERS", $supplier);
+        if ($supplier == "COOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS")
+            $supplier = str_replace("COOPERSCOOPERSCOOPERSFIAAM FILTERS FILTERS FILTERS", "COOPERSFIAAM FILTERS", $supplier);
+        if ($supplier == "COOPERSCOOPERSFIAAM FILTERS FILTERS")
+            $supplier = str_replace("COOPERSCOOPERSFIAAM FILTERS FILTERS", "COOPERSFIAAM FILTERS", $supplier);
+        if ($supplier == "CoopersFiaam")
+            $supplier = str_replace("CoopersFiaam", "COOPERSFIAAM FILTERS", $supplier);
+        if ($supplier == "MANN")
+            $supplier = str_replace("MANN", "MANN-FILTER", $supplier);
+        if ($supplier == "MANN-FILTER-FILTER-FILTER-FILTER")
+            $supplier = str_replace("MANN-FILTER-FILTER-FILTER-FILTER", "MANN-FILTER", $supplier);
+        if ($supplier == "MANN-FILTER-FILTER")
+            $supplier = str_replace("MANN-FILTER-FILTER", "MANN-FILTER", $supplier);
+        if ($supplier == "MANN-FILTER-FILTER")
+            $supplier = str_replace("MANN-FILTER-FILTER", "MANN-FILTER", $supplier);
+        if ($supplier == "MANN-FILTEREX")
+            $supplier = str_replace("MANN-FILTEREX", "MANN-FILTER", $supplier);
+        if ($supplier == "METALCAUCHO")
+            $supplier = str_replace("METALCAUCHO", "Metalcaucho", $supplier);
+        if ($supplier == "MULLER")
+            $supplier = str_replace("MULLER", "MULLER FILTER", $supplier);
+        if ($supplier == "RICAMBI")
+            $supplier = str_replace("RICAMBI", "GENERAL RICAMBI", $supplier);
+        if ($supplier == "ZIMMERMANN-FILTER")
+            $supplier = str_replace("VERNET", "CALORSTAT by Vernet", $supplier);
+        if ($supplier == "ZIMMERMANN-FILTER")
+            $supplier = str_replace("ZIMMERMANN-FILTER", "ZIMMERMANN", $supplier);
+        if ($supplier == "LESJ?FORS")
+            $supplier = str_replace("LESJ?FORS", "LESJOFORS", $supplier);
+        if ($supplier == "LEMF?RDER")
+            $supplier = str_replace("LEMF?RDER", "LEMFORDER", $supplier);
+        if ($supplier == "SALERI")
+            $supplier = str_replace("SALERI", "Saleri SIL", $supplier);
+        if ($supplier == "CASTROL LUBRICANTS")
+            $supplier = str_replace("CASTROL LUBRICANTS", "CASTROL", $supplier);
+        if ($supplier == "FEBI BILSTEIN BILSTEIN")
+            $supplier = str_replace("FEBI BILSTEIN BILSTEIN", "FEBI BILSTEIN", $supplier);
+        if ($supplier == "KM Germany Germany")
+            $supplier = str_replace("KM Germany Germany", "KM Germany", $supplier);
+        return $supplier;
+    }
 
     /**
      * @var string
