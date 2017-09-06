6f1468cbe1a530cc53a0165d0d5ba9801ef6c2c7
AlexanderTheGr <a.dimeas@gmail.com>
2017-09-06 11:01:30 +0300

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1877,6 +1877,34 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
     }
 
     
+    /**
+     * 
+     * 
+     * @Route("/product/getSoftoneSuppliers")
+     */
+    public function getSoftoneSuppliers(Request $request) {
+        //echo 'ssss';
+        $allowedips = $this->getSetting("SoftoneBundle:Product:Allowedips");
+        $allowedipsArr = explode(",", $allowedips);
+        if (in_array($_SERVER["REMOTE_ADDR"], $allowedipsArr)) {
+            $sql = "SELECT * FROM  `softone_softone_supplier`";
+            $connection = $this->getDoctrine()->getConnection();
+            $statement = $connection->prepare($sql);
+            $statement->execute();
+            $results = $statement->fetchAll();
+            $arr = array();
+            foreach ($results as $data) {
+                $arr[] = $data;
+            }
+            $json = json_encode($arr);
+            return new Response(
+                    $json, 200, array('Content-Type' => 'application/json')
+            );
+        } else {
+         //   
+            exit;
+        }
+    } 
     
     /**
      * 
