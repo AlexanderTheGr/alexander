14dd3432949817be2c06a202fcd9b81b6da2536e
AlexanderTheGr <a.dimeas@gmail.com>
2017-09-07 15:20:24 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1021,6 +1021,7 @@ class ProductController extends Main {
         $cars = (array) $product->getCars();
         //echo $product->cars;
         //print_r($cars);
+        
         $html = "<ul class='pbrands' data-prod='" . $product->getId() . "'>";
         foreach ($brands as $brand) {
             $style1 = "";
