fdeef9cb74e9567b9367b6538c59d65057f2c013
AlexanderTheGr <a.dimeas@gmail.com>
2017-09-07 15:35:25 +0300

    ...

--- src/AppBundle/Controller/Main.php
+++ src/AppBundle/Controller/Main.php
@@ -87,7 +87,7 @@ class Main extends Controller {
                     if (count($field_relation) == 1) {
                         if ($this->clearstring($dt_search["value"]) != "") {
                             $this->q_or[] = $this->prefix . "." . $field["index"] . " LIKE '%" . $this->clearstring($dt_search["value"]) . "%'";
-                            $this->q_or[] = $this->prefix . "." . $field["index"] . " LIKE '%" . $dt_search["value"] . "%'";
+                            //$this->q_or[] = $this->prefix . "." . $field["index"] . " LIKE '%" . $dt_search["value"] . "%'";
                         }
                         if (@$field["method"] == 'yesno') {
                             if (@$this->clearstring($dt_columns[$index]["search"]["value"]) == "0") {
@@ -99,7 +99,7 @@ class Main extends Controller {
                         } else {
                             if (@$this->clearstring($dt_columns[$index]["search"]["value"]) != "") {
                                 $this->q_and[] = $this->prefix . "." . $this->fields[$index]["index"] . " LIKE '%" . $this->clearstring($dt_columns[$index]["search"]["value"]) . "%'";
-                                $this->q_and[] = $this->prefix . "." . $this->fields[$index]["index"] . " LIKE '%" . $dt_columns[$index]["search"]["value"] . "%'";
+                                //$this->q_and[] = $this->prefix . "." . $this->fields[$index]["index"] . " LIKE '%" . $dt_columns[$index]["search"]["value"] . "%'";
                             }
                         }
                         $s[] = $this->prefix . "." . $field_relation[0];
@@ -110,7 +110,7 @@ class Main extends Controller {
                         if ($dt_search["value"] === true) {
                             if ($this->clearstring($dt_search["value"]) != "") {
                                 $this->q_or[] = $this->prefix . "." . $field_relation[0] . " = '" . $this->clearstring($dt_search["value"]) . "'";
-                                $this->q_or[] = $this->prefix . "." . $field_relation[0] . " = '" . $dt_search["value"] . "'";
+                                //$this->q_or[] = $this->prefix . "." . $field_relation[0] . " = '" . $dt_search["value"] . "'";
                             }
                         }
                         if (@$this->clearstring($dt_columns[$index]["search"]["value"]) != "") {
