e356cbda367a32c1b1b742af50b8f589d669dca2
AlexanderTheGr <a.dimeas@gmail.com>
2017-09-18 15:11:37 +0300

    ...

--- src/EdiBundle/Controller/EdiController.php
+++ src/EdiBundle/Controller/EdiController.php
@@ -331,7 +331,7 @@ class EdiController extends Main {
         $i = 0;
         foreach ($collection as $entity) {
             //if ($i++ <= 1) continue;
-            if ($entity["id"] == 1) {
+            if ($entity["id"] == 6) {
                 $func = $entity["func"];
                 $this->$func($entity);
             }
