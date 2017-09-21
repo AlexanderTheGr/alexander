22e3f54316ce6c480031cd2f9f39a3d49dda9e4b
AlexanderTheGr <a.dimeas@gmail.com>
2017-09-21 10:53:51 +0300

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -461,6 +461,8 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
         //$dtparams[] = array("name" => "EDI", "index" => 'edi', "input" => 'text', 'search' => 'text');
 
         $dtparams[] = array("name" => "-", "function" => 'getTick', 'search' => 'text');
+        
+        $dtparams[] = array("name" => "-", "function" => 'getEdiPrices', 'search' => 'text');
 
         //$dtparams[] = array("name" => "ID", "function" => 'getTest', "active" => "active");
         $params['dtparams'] = $dtparams;
