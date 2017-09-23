ba7d91edee67cd2edbf8f48fa59d51a450ced261
alexanderthegr <a.dimeas@gmail.com>
2017-09-23 11:51:45 +0300

    *-

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1705,7 +1705,7 @@ class ProductController extends Main {
         }
         //ini_set("soap.wsdl_cache_enabled", "0");
         //exit;
-
+        /*
         $ch = \curl_init();
         $header = array('Contect-Type:application/xml', 'Accept:application/xml');
         curl_setopt($ch, CURLOPT_URL, "http://wsprisma.megasoft.gr/mgsft_ws.asmx/DownloadStoreBase");
@@ -1721,6 +1721,7 @@ class ProductController extends Main {
         $server_output = curl_exec($ch);
         $databale = @explode(".", $_SERVER["HTTP_HOST"]);
         file_put_contents("downliad_" . $databale[0] . ".xml", $server_output);
+        */
 
         //$StoreDetails = \simplexml_load_string($server_output);
         $result = \simplexml_load_file("downliad_" . $databale[0] . ".xml");
