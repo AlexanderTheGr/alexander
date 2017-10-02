ff85bd84737fb9fd161e9ec64bc46a3d92237ba3
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-02 12:56:26 +0300

    ...

--- src/MegasoftBundle/Controller/ProductController.php
+++ src/MegasoftBundle/Controller/ProductController.php
@@ -1771,6 +1771,7 @@ class ProductController extends Main {
             //if ($i++ < ($cnt-10000))
             //   continue;
             $i++;
+            $storeIds = array();
             if ($i > 50000 AND $i<100000) {            
                 $data = (array) $data;
                 $storeIds[] = array("storeid"=>addslashes($data["StoreId"]));
