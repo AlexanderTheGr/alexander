f238f8c8f479d27e6662fb4c78e6500a1bda2e6c
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-05 16:48:34 +0300

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -1350,10 +1350,10 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
         $out = "<ul style='width:100%; float:left;'>";
         foreach ($brands as $brand) {
             $urlpath = str_replace("/home2/partsbox/public_html/partsbox/web", "", $path);
-            $out .= "<li class='fgogo' data-ref='".$brand["model_id"]."' style='width:120px; height: 120px; float: left; list-style: none'>"
+            $out .= "<li style='width:120px; height: 120px; float: left; list-style: none'>"
                     . "<div style='float:left; width:100%' class='modeldiv'><img class='modelitem' style='border: 1px; z-index:100; position:absolute; display: none; left:0;' style='max-width:820px; max-height: 820px;' src='" . $urlpath . "cars" . "/" . $brand["model_id"] . ".jpg'>"
                      . "<center><img style='max-width:120px; max-height: 120px;' src='" . $urlpath . "cars" . "/" . $brand["model_id"] . ".jpg'></div>"
-                     . "<BR><BR><center><a href='#'>".$brand["year"]."</a></center></li>";
+                     . "<BR><BR><center><a class='fgogo' data-ref='".$brand["model_id"]."' href='#'>".$brand["year"]."</a></center></li>";
         }
         $out .= "</ul>";
         return $out;
