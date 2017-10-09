534a8aacb31e816e6e9eab009a8df39b0fc5dd15
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-09 12:52:32 +0300

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -2774,7 +2774,7 @@ class Product extends Entity {
         foreach ($datas as $catalogue => $packs) {
             foreach ($packs as $k => $data) {
                 $data_string = json_encode($data);
-
+                echo $data_string;
                 //continue;
                 $result = file_get_contents($requerstUrl, null, stream_context_create(array(
                     'http' => array(
