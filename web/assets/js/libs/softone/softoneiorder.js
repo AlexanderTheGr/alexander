277b373f7681798be5b53b5e20a9295749fc2299
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-09 12:24:17 +0300

    ...

--- web/assets/js/libs/softone/softoneiorder.js
+++ web/assets/js/libs/softone/softoneiorder.js
@@ -535,13 +535,15 @@ jQuery(".alexander tr").live('click', function () {
     var data = {}
     data.id = jQuery(this).attr("data-ref");
     //jQuery(this).find('.orderitemstable').show();
-    jQuery('.ediprices').hide();
-    var obj = this;
-    jQuery.post("/product/getediprices", data, function (result) {
-        jQuery(obj).find('.ediprices').html(result);
-        jQuery(obj).find('.ediprices').show();
-        jQuery(obj).find('.orderitemstable').show();
-    })  
+    if (data.id > 0 && jQuery(this).find('.ediprices') != '') {
+        jQuery('.ediprices').hide();
+        var obj = this;
+        jQuery.post("/product/getediprices", data, function (result) {
+            jQuery(obj).find('.ediprices').html(result);
+            jQuery(obj).find('.ediprices').show();
+            jQuery(obj).find('.orderitemstable').show();
+        })
+    }
 });
 
 jQuery(".alexander tr").live('mouseover', function () {
