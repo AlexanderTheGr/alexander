e74d2ed62f336c0637ee98b7a72917c63124c847
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-11 14:21:35 +0300

    ...

--- web/assets/js/libs/alexander/alexDataTable.js
+++ web/assets/js/libs/alexander/alexDataTable.js
@@ -59,7 +59,7 @@
                 }
                 location.href = view + "/" + $(this).attr("ref");
             });
-            jQuery("select.search_init").chosen();
+            jQuery("select.search_init").chosen({width: "100px"});
             $(".btn_new_"+ctrl).live('click', function () {
                 location.href = view + "/new";
             });
