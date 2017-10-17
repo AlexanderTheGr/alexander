238ecc17874bddef6df8c624e94b4a6f3c155cad
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-17 13:38:09 +0300

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -2861,7 +2861,7 @@ class Product extends Entity {
         $url = $this->getFanoImageUrl();
         if (file_exists($url)) {
             $urlpath = str_replace("/home2/partsbox/public_html/partsbox/web", "", $url);
-            return '<div class="productfanoimg productfanoimg_' . $this->id . '"><img src="'.$urlpath.'" /></div>';
+            return '<div style="display:none; position: absolute;" class="productfanoimg productfanoimg_' . $this->id . '"><img src="'.$urlpath.'" /></div>';
         }
     }
 
