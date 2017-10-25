4354bba0815322ab2965fe7ce40c4e5934c9c923
alexanderthegr <a.dimeas@gmail.com>
2017-10-25 22:41:51 +0300

    *-

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1672,10 +1672,10 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                                     . "code_adv = '" . $data[6] . "',"
                                     . "oem = '" . $oem . "'";
                             //echo $sql . "<BR>";
-                            //if ($k++ > 100) {
-                            echo ".";
-                            //$k = 0;
-                            //}
+                            if ($k++ > 100) {
+                                echo ".";
+                                $k = 0;
+                            }
                             $em->getConnection()->exec($sql);
                         }
                     }
