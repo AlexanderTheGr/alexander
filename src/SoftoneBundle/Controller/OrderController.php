7d066b04dab80d28ab911ac9766283cdb5876ac7
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-27 10:38:33 +0300

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -392,7 +392,8 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
         $dtparams[] = array("name" => $this->getTranslation("Product Name"), "index" => 'product:title');
         $dtparams[] = array("name" => $this->getTranslation("Product Code"), "index" => 'product:erpCode');
         $dtparams[] = array("name" => $this->getTranslation("Place"), "index" => 'product:itemMtrplace');
-        $dtparams[] = array("name" => $this->getTranslation("Supplier"), "index" => 'product:supplierId:title');
+        //$dtparams[] = array("name" => $this->getTranslation("Supplier"), "index" => 'product:supplierId:title');
+        $dtparams[] = array("name" => $this->getTranslation("Supplier"), "function" => 'getForOrderSupplier', 'search' => 'text');
         $dtparams[] = array("name" => $this->getTranslation("Invetory"), "function" => 'getProductApothiki', 'search' => 'text');
         $dtparams[] = array("name" => $this->getTranslation("Qty"), "input" => "text", "index" => 'qty');
         $dtparams[] = array("name" => $this->getTranslation("Catalogue Price"), "input" => "text", "index" => 'price');
