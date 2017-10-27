a547a6af17d91a260f625ae4f2e2b3d001ccdf1f
AlexanderTheGr <a.dimeas@gmail.com>
2017-10-27 15:20:36 +0300

    ...

--- src/SoftoneBundle/Entity/Orderitem.php
+++ src/SoftoneBundle/Entity/Orderitem.php
@@ -252,7 +252,7 @@ class Orderitem {
     public function getForOrderItemsTitle() {
 
 
-        $out = '<a title="' . $this->getProduct()->getTitle() . '" class="productfano_info" car="" data-articleId="' . $this->getProduct()->getTecdocArticleId() . '" data-ref="' . $this->getProduct()->getId() . '" href="#">' . $this->getProduct()->getTitle() . '</a>';
+        $out = '<a title="' . $this->getProduct()->getTitle() . '" class="productfano_info" car="" data-articleId="' . $this->getProduct()->getTecdocArticleId() . '" data-ref="' . $this->getProduct()->getId() . '">' . $this->getProduct()->getTitle() . '</a>';
         $out .= '<div class="ediprices ediprices_' . $this->getProduct()->getId() . '"></div>';
         if ($this->remarks)
         $out .=  '<BR><span class="text-sm text-info">' . $this->remarks . '</span>';// "<BR>".$this->remarks;
