aa4d2936dddfb6f47c5afac30ed38393e46ad686
AlexanderTheGr <a.dimeas@gmail.com>
2017-11-01 16:27:07 +0200

    ...

--- web/assets/js/libs/softone/softoneiorder.js
+++ web/assets/js/libs/softone/softoneiorder.js
@@ -470,6 +470,7 @@ jQuery('.SoftoneBundleGBGProductAdd').live("click", function (e) {
         data.product = jQuery(this).attr('data-id');
         var store = data.product;
         data.qty = 1;
+        data.price = jQuery(this).attr('data-price');
         data.store = jQuery(this).attr('data-qty')//jQuery("#store_" + store).val();
         $("#loaderer").show();
         $.post("/edi/order/addorderitem/", data, function (result) {
