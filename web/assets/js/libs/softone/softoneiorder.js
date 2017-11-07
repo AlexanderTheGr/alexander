94e553df67607831ed3761c0db596574bf57e167
AlexanderTheGr <a.dimeas@gmail.com>
2017-11-07 12:52:50 +0200

    ...

--- web/assets/js/libs/softone/softoneiorder.js
+++ web/assets/js/libs/softone/softoneiorder.js
@@ -158,7 +158,44 @@ jQuery(".SoftoneBundleOrderitemQty").live('keyup', function (e) {
         })
     }
 })
-
+//SoftoneBundleInvoiceItemPrice 
+jQuery(".SoftoneBundleInvoiceItemPrice").live('keyup', function (e) {
+    if (e.keyCode == 13) {
+        var data = {}
+        data.id = jQuery(this).attr('data-id');
+        data.price = jQuery(this).val();
+        $("#loaderer").show();
+        $.post("/invoice/editinvoiceitem/", data, function (result) {
+            $("#loaderer").hide();
+            var json = angular.fromJson(result);
+            if (json.error) {
+                toastr.error(json.message, "Error");
+            }
+            var table = dt_tables["ctrlgettabs"];
+            //$(".offcanvas-search").click();
+            table.fnFilter();
+        })
+    }
+})
+jQuery(".SoftoneBundleInvoiceItemQty").live('keyup', function (e) {
+    if (e.keyCode == 13) {
+        var data = {}
+        data.id = jQuery(this).attr('data-id');
+        data.qty = jQuery(this).val();
+        $("#loaderer").show();
+        $.post("/invoice/editinvoiceitem/", data, function (result) {
+            $("#loaderer").hide();
+            var json = angular.fromJson(result);
+            if (json.error) {
+                toastr.error(json.message, "Error");
+            }
+            var table = dt_tables["ctrlgettabs"];
+            //$(".offcanvas-search").click();
+            table.fnFilter();
+        })
+    }
+})
+ 
 
 
 
