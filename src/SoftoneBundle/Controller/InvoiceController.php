6817d43a54d90893e25335d1f6d2adf514235447
AlexanderTheGr <a.dimeas@gmail.com>
2017-11-07 15:41:51 +0200

    ...

--- src/SoftoneBundle/Controller/InvoiceController.php
+++ src/SoftoneBundle/Controller/InvoiceController.php
@@ -365,7 +365,7 @@ class InvoiceController extends \SoftoneBundle\Controller\SoftoneController {
             $objectArr[0]["INT01"] = $order->getUser()->getReference();
         }
         */
-        $objectArr[0]["SERIES"] = 7021;//$order->getSoftoneStore()->getSeries();
+        $objectArr[0]["SERIES"] = 7121;//$order->getSoftoneStore()->getSeries();
         $objectArr[0]["VATSTS"] = $vatsst;//$this->getSetting("SoftoneBundle:Order:Vat") != '' ? $this->getSetting("SoftoneBundle:Order:Vat") : $customer->getCustomerVatsts();
         $objectArr[0]["COMMENTS"] = "";//$order->getRemarks(); //$customer->getCustomerPayment() > 0 ? $customer->getCustomerPayment() : 1003; // Mage::app()->getRequest()->getParam('comments');
         $objectArr[0]["REMARKS"] = "";//$order->getRemarks();
