fbdda3a409567efbc840e59fabc00dc266e5b5ef
AlexanderTheGr <a.dimeas@gmail.com>
2017-11-07 12:47:32 +0200

    ...

--- src/SoftoneBundle/Entity/InvoiceItem.php
+++ src/SoftoneBundle/Entity/InvoiceItem.php
@@ -259,5 +259,8 @@ class InvoiceItem {
     public function getProductApothiki() {
         return $this->getProduct()->getApothiki();
         //return $this->lineval / $this->qty;
+    }
+    public function deleteitem() {
+        return '<a style="font-size:20px; color:red; cursor: pointer" data-id="' . $this->id . '" class="deleteitem"><i class="md md-delete"></i></a>';
     }    
 }
