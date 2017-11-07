7608a52c0442636b1496fc651f7138923a77a2b8
AlexanderTheGr <a.dimeas@gmail.com>
2017-11-07 11:55:13 +0200

    ...

--- src/SoftoneBundle/Controller/InvoiceController.php
+++ src/SoftoneBundle/Controller/InvoiceController.php
@@ -7,11 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Controller\Main as Main;
 
-
-class InvoiceController extends \SoftoneBundle\Controller\SoftoneController  {
+class InvoiceController extends \SoftoneBundle\Controller\SoftoneController {
 
     var $repository = 'SoftoneBundle:Route';
     var $newentity = '';
+
     /**
      * @Route("/invoice/invoice")
      */
@@ -89,10 +89,59 @@ class InvoiceController extends \SoftoneBundle\Controller\SoftoneController  {
 
         $forms = $this->getFormLyFields($entity, $fields);
 
+        if ($id > 0 AND count($entity) > 0) {
+            $dtparams[] = array("name" => "ID", "index" => 'id', "active" => "active");
+            $dtparams[] = array("name" => "Title", "index" => 'km');
+            //$dtparams[] = array("name" => "Title", "index" => 'km');
+            //$dtparams[] = array("name" => "Price", "index" => 'storeWholeSalePrice');
+            $params['dtparams'] = $dtparams;
+            $params['id'] = $dtparams;
+            $params['url'] = '/invoice/invoice/getitems/' . $id;
+            $params['view'] = '/invoice/invoice/item/view';
+            $params['viewnew'] = '/invoice/invoice/item/view/new/' . $id;
+
+            $params['key'] = 'gettabs_' . $id;
+            $params["ctrl"] = 'ctrlgettabs';
+            $params["app"] = 'appgettabs';
+            $datatables[] = $this->contentDatatable($params);
+        }
+
         $this->addTab(array("title" => $this->getTranslation("General"), "form" => $forms, "content" => '', "index" => $this->generateRandomString(), 'search' => 'text', "active" => true));
+        if ($id > 0 AND count($entity) > 0) {
+            $tabs[] = array("title" => $this->getTranslation("Items"), "datatables" => $datatables, "form" => '', "content" => '', "index" => $this->generateRandomString(), 'search' => 'text', "active" => false);
+        }        
         $json = $this->tabs();
         return $json;
     }
+    
+    /**
+     * @Route("/invoice/invoice/getitems/{id}")
+     */
+    public function getservicesAction($id) {
+        $session = new Session();
+        foreach ($session->get('params_gettabs_' . $id) as $param) {
+            $this->addField($param);
+        }
+        $this->repository = 'SoftoneBundle:InvoiceItem';
+        $this->q_and[] = $this->prefix . ".invoice = '" . $id . "'";
+        $json = $this->datatable();
+
+        $datatable = json_decode($json);
+        $datatable->data = (array) $datatable->data;
+        foreach ($datatable->data as $key => $table) {
+            $table = (array) $table;
+            $table1 = array();
+            foreach ($table as $f => $val) {
+                $table1[$f] = $val;
+            }
+            $datatable->data[$key] = $table1;
+        }
+        $json = json_encode($datatable);
+        return new Response(
+                $json, 200, array('Content-Type' => 'application/json')
+        );
+    }
+    
 
     /**
      * @Route("/invoice/getdatatable")
