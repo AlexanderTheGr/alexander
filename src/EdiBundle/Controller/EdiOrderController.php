b4c23071e2e1d78c55bd3e66eff8643d32c18549
AlexanderTheGr <a.dimeas@gmail.com>
2017-11-14 13:18:25 +0200

    ...

--- src/EdiBundle/Controller/EdiOrderController.php
+++ src/EdiBundle/Controller/EdiOrderController.php
@@ -296,14 +296,14 @@ class EdiOrderController extends Main {
             $EdiOrderItem->setField("chk", 1);
             try {
                 $this->flushpersist($EdiOrderItem);
-                $json = json_encode(array("error" => false, "product"=>$product,"message" => $Ediitem->getEdi()->getName() . " " . $Ediitem->getItemCode() . " ανοιχτηκε επιτυχώς"));
+                $json = json_encode(array("error" => false, "product"=>$product->getId(),"message" => $Ediitem->getEdi()->getName() . " " . $Ediitem->getItemCode() . " ανοιχτηκε επιτυχώς"));
             } catch (\Exception $e) {
                 $json = json_encode(array("error" => true, "message" => "Product Exists"));
             }
         } else {
             try {
                 //$this->flushpersist($EdiOrderItem);
-                $json = json_encode(array("error" => false, "message" => $Ediitem->getEdi()->getName() . " " . $Ediitem->getItemCode() . " ανοιχτηκε επιτυχώς"));
+                $json = json_encode(array("error" => false, "product"=>$product->getId(), "message" => $Ediitem->getEdi()->getName() . " " . $Ediitem->getItemCode() . " ανοιχτηκε επιτυχώς"));
             } catch (\Exception $e) {
                 $json = json_encode(array("error" => true, "message" => "Product Exists"));
             }
