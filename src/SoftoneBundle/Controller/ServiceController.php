aee090f576e0947e94ca61b942f3e6b4629f6028
AlexanderTheGr <a.dimeas@gmail.com>
2017-11-29 10:04:05 +0200

    ...

---
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SoftoneBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use SoftoneBundle\Entity\Category as Category;
+use AppBundle\Controller\Main as Main;
+
+class ServiceController extends Main{
+
+    var $repository = 'SoftoneBundle:Category';
+
+    /**
+     * @Route("/service/service")
+     */
+    public function indexAction() {
+
+        
+        $content = "aaaaa";
+        
+        return $this->render('SoftoneBundle:Category:index.html.twig', array(
+                    'pagename' => 'Service',
+                    'url' => '/category/getdatatable',
+                    'view' => '/category/view',
+                    'ctrl' => $this->generateRandomString(),
+                    'app' => $this->generateRandomString(),
+                    'content' => $content,
+                    'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
+        ));
+    }
+
+
+}
