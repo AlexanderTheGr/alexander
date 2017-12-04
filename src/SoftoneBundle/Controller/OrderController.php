bb2e25da3fdaf327d791c093e405c72e38ae6a42
AlexanderTheGr <a.dimeas@gmail.com>
2017-12-04 11:04:09 +0200

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -855,7 +855,7 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
                                 ORDER BY ' . $this->orderBy;
 
                     if ($search[1] != '') {
-                        $sqlearch2 = $sqlearch;
+                        $sqlearch2 = "p.id in (".$sqlearch.") OR";
                         //$sqlearch2 = "p.id in (Select o.id from SoftoneBundle:ProductSearch o where o.search like '%" . $search[1] . "%') OR ";
                     }
                     $hasArticleIds = true;
