2cf76cc3aac50fb140bd36f2b4c65ba8c187ee1a
AlexanderTheGr <a.dimeas@gmail.com>
2017-12-12 13:33:53 +0200

    ...

--- src/SoftoneBundle/Controller/InvoiceController.php
+++ src/SoftoneBundle/Controller/InvoiceController.php
@@ -338,7 +338,7 @@ class InvoiceController extends \SoftoneBundle\Controller\SoftoneController {
         $total = 0;
         foreach ($data->data as $item) {
 
-            $of = "7";
+            $of = "8";
 
             $text = $item->$of;
             $document = new \DOMDocument();
