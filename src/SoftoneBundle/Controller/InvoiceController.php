4a43c5167d000aef6fd53f5823f3dc566b8e3445
AlexanderTheGr <a.dimeas@gmail.com>
2017-12-12 11:59:58 +0200

    ...

--- src/SoftoneBundle/Controller/InvoiceController.php
+++ src/SoftoneBundle/Controller/InvoiceController.php
@@ -375,13 +375,15 @@ class InvoiceController extends \SoftoneBundle\Controller\SoftoneController {
           $vatsst = 1410; //$this->getSetting("SoftoneBundle:Product:Vat");
          */
         $vatsst = 1410;
-
+        /*
         if ($invoice->getReference() > 0) {
             $json = json_encode(array());
             return new Response(
                     $json, 200, array('Content-Type' => 'application/json')
             );
         }
+         * 
+         */
 
         $objectArr = array();
         $objectArr[0]["TRDR"] = $invoice->getSupplier()->getReference(); //9818; //$customer->getReference();
