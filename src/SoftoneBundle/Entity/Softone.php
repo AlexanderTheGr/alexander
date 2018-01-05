3327bdf56a933cc4139747052be3b865e006783f
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-05 13:30:02 +0200

    ...

--- src/SoftoneBundle/Entity/Softone.php
+++ src/SoftoneBundle/Entity/Softone.php
@@ -151,7 +151,7 @@ class Softone extends Entity {
                 'method' => 'POST',
                 'header' =>
                 'Content-Type: application/json' . "\r\n"
-                . 'Content-Length: ' . (strlen($data_string)+1) . "\r\n",
+                . 'Content-Length: ' . (strlen($data_string)) . "\r\n",
                 'content' => $data_string,
             ),
         )));
