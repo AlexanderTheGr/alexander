f1bf2058d4e2f9337db064902d1199fae4a6f05c
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-08 10:18:45 +0200

    ...

--- src/SoftoneBundle/Entity/Softone.php
+++ src/SoftoneBundle/Entity/Softone.php
@@ -138,18 +138,7 @@ class Softone extends Entity {
     function doRequest2($data, $requerstUrl = false) {
         $requerstUrl = $requerstUrl ? $requerstUrl : $this->requerstUrl;
         ini_set('memory_limit', '2048M');
-        $data_string = json_encode($data);
-        //$data_string = json_encode($data);
-        $ch = curl_init($requerstUrl);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Content-Length: ' . strlen($data_string))
-        );
-
-        $result = curl_exec($ch);
+        $result = file_get_contents($requerstUrl);
         echo $result;
         exit;
         
