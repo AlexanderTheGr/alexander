0e37cb5530af18d592302f3fd33c82c5ab7e773a
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-09 14:25:04 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1244,8 +1244,8 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                     $where = " AND UPDDATE >= '" . date("Y-m-d", strtotime("-1 days")) . "' ORDER BY MTRL";
                 } else {
 
-                    $MTRL1 = 105000;
-                    $MTRL2 = 120000;
+                    $MTRL1 = 120000;
+                    $MTRL2 = 150000;
 
                     $where = " AND  MTRL >= " . $MTRL1 . " AND MTRL < " . $MTRL2 . "  ORDER BY MTRL";
                 }
