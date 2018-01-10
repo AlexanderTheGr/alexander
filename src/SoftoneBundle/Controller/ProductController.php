1e5a603da07965aafe2696ca7d07bb1982c73ac8
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-10 15:07:57 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -2217,7 +2217,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
             echo "<BR>" . count($datas) . "<BR>";
             //exit;
         } else {
-            $filters = "ITEM.MTRL=0&ITEM.MTRL_TO=20000"; 
+            $filters = "ITEM.MTRL=20000&ITEM.MTRL_TO=80000"; 
             $datas = $softone->retrieveData("ITEM", "apothema",$filters);
             echo "<BR>" . count($datas) . "<BR>";
             //print_r($datas);
