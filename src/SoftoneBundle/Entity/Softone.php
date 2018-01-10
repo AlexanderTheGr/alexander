ebb63386c38f6a89d27f2921208deb6fdce56edc
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-10 12:48:42 +0200

    ...

--- src/SoftoneBundle/Entity/Softone.php
+++ src/SoftoneBundle/Entity/Softone.php
@@ -151,9 +151,10 @@ class Softone extends Entity {
                 'content' => $data_string,
             ),
         )));
-        echo $result;
+        
         if (@$result1 = gzdecode($result)) {
             $result = iconv("ISO-8859-7", "UTF-8", $result1);
+            echo $result;
         } else {
             $result = iconv("ISO-8859-7", "UTF-8", $result);
         }
