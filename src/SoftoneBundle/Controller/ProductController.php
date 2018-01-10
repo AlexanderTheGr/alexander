37a940e03e4aa3c9ad12daec35be4c795558c609
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-10 14:31:26 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -2226,7 +2226,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         //$datas = $softone->retrieveData("ITEM", "apothema");
         //echo 'Sss';
         echo count($datas) . "<BR>";
-        print_r($datas);
+        //print_r($datas);
         //exit;
         $em = $this->getDoctrine()->getManager();
 
