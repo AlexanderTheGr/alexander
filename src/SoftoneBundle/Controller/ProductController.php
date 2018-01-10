9293fdb3375cf03bf725c4b801aa9ba5289d98f5
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-10 15:04:47 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -2217,7 +2217,8 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
             echo "<BR>" . count($datas) . "<BR>";
             //exit;
         } else {
-            $datas = $softone->retrieveData("ITEM", "apothema");
+            $filters = "ITEM.UPDDATE=".date("Y-m-d")."&ITEM.UPDDATE_TO=".date("Y-m-d"); 
+            $datas = $softone->retrieveData("ITEM", "apothema",$filters);
             echo "<BR>" . count($datas) . "<BR>";
             //print_r($datas);
             //exit;
