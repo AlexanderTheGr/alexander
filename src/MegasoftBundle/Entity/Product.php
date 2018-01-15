5643516e6a657a55aa0e1e48184765bb4eceb8eb
alexanderthegr <a.dimeas@gmail.com>
2018-01-15 16:58:44 +0200

    *-

--- src/MegasoftBundle/Entity/Product.php
+++ src/MegasoftBundle/Entity/Product.php
@@ -1135,7 +1135,7 @@ class Product extends Entity {
         $params["JsonStrWeb"] = json_encode($data);
         //print_r($params);
         $response = $soap->__soapCall("SetProduct", array($params));
-        //print_r($response);
+        print_r($response);
         if ($response->SetProductResult > 0 AND $this->reference == 0) {
             $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
             $this->reference = $response->SetProductResult;
