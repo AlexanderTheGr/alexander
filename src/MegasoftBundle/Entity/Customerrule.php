861947185054606297ea480757371f45bdf451f7
AlexanderTheGr <a.dimeas@gmail.com>
2018-01-29 13:18:33 +0200

    ...

--- src/MegasoftBundle/Entity/Customerrule.php
+++ src/MegasoftBundle/Entity/Customerrule.php
@@ -232,7 +232,7 @@ class Customerrule {
     }
 
     function rulesLoop($rule, $catsEp, $supplier, $code, $productsale,$supplier2) {
-        //print_r($rule["rules"]);
+        print_r($rule["rules"]);
         foreach ($rule["rules"] as $rl) {
             print_r($r1);
             if (count($rl["rules"])) {
