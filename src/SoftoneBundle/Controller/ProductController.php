5aeee55826b026f8bdcbc5410eff9d32b290aa73
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-07 11:28:32 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1272,10 +1272,8 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
             $params["softone_table"] = 'MTRL';
             $params["table"] = 'softone_product';
             $params["object"] = 'SoftoneBundle\Entity\Product';
-            $company = $this->getSetting("SoftoneBundle:Softone:company") > 0 ? $this->getSetting("SoftoneBundle:Softone:company") : 1000;
-            $sodtype = 51;//$this->getSetting("SoftoneBundle:Softone:apothiki") == 'carparts' ? 61 : 51;
             if ($this->getSetting("SoftoneBundle:Softone:merchant") == 'gianop') {
-                $params["filter"] = 'WHERE M.SODTYPE='.$sodtype.' ' . $where;
+                $params["filter"] = 'WHERE M.SODTYPE=51 ' . $where;
                 //$params["filter"] = "WHERE M.SODTYPE=51 AND M.CODE2 = '19050'";
                 //$extra["foxline"] = array("CCCFXRELTDCODE" => "CCCFXRELTDCODE", "CCCFXRELBRAND" => "CCCFXRELBRAND");
                 $params["extra"] = array(); //$this->getSetting("SoftoneBundle:Softone:merchant") ? $extra[$this->getSetting("SoftoneBundle:Softone:merchant")] : array("cccRef" => "cccRef", "cccWebUpd" => "cccWebUpd", "cccPriceUpd" => "cccPriceUpd");
@@ -1288,7 +1286,7 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
                     $extra["foxline"] = array("CCCFXRELTDCODE" => "CCCFXRELTDCODE", "CCCFXRELBRAND" => "CCCFXRELBRAND");
                 }
                 $extra["foxline"] = array("CCCFXRELTDCODE" => "CCCFXRELTDCODE");
-                $params["filter"] = 'WHERE M.SODTYPE='.$sodtype.' ' . $where;
+                $params["filter"] = 'WHERE M.SODTYPE=51 ' . $where;
                 $params["extra"] = $this->getSetting("SoftoneBundle:Softone:merchant") ? $extra[$this->getSetting("SoftoneBundle:Softone:merchant")] : array("cccRef" => "cccRef", "cccWebUpd" => "cccWebUpd", "cccPriceUpd" => "cccPriceUpd");
             }
             $params["relation"] = array();
@@ -1397,21 +1395,9 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         //$sql = "SELECT M.MTRL,M.INSDATE,M.UPDDATE,M.ISACTIVE,M.VAT,M.MTRMANFCTR,M.MTRMARK,M.REMARKS,M.MARKUPW,M.MARKUPR,M.PRICER,M.PRICEW,M.PRICEW01,M.PRICEW02,M.PRICEW03,M.PRICEW04,M.PRICEW05,M.PRICER01,M.PRICER02,M.PRICER03,M.PRICER04,M.PRICER05,M.NAME,M.NAME1,M.CODE,M.CODE1,M.CODE2,M.APVCODE,M.MTRPLACE,M.MTRSUP,M.MTRCATEGORY,M.MTRGROUP FROM MTRL M WHERE M.SODTYPE=51 AND MTRL = 421443";
         // echo "<BR>";
         //echo $params["fSQL"];
-       // echo $params["fSQL"];
+        echo $params["fSQL"];
         // echo "<BR>";
         //return;
-        /*
-        if ($this->getSetting("SoftoneBundle:Softone:apothiki") == 'carparts') {
-            $params["fSQL"] = "SELECT * FROM MTRL M WHERE COMPANY = 1001 AND MTRL < 4500";
-            echo $params["fSQL"];
-            $softone = new Softone();
-            $datas = $softone->createSql($params);  
-            print_r($datas);
-            exit;            
-        }
-        
-        exit;
-        */
         $softone = new Softone();
         $datas = $softone->createSql($params);
         echo count($datas->data);
