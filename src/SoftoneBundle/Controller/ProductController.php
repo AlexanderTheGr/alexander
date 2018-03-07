b2ab1d352d219a68c98703be142c6346a4f73dbb
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-07 11:06:27 +0200

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1395,11 +1395,12 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
         //$sql = "SELECT M.MTRL,M.INSDATE,M.UPDDATE,M.ISACTIVE,M.VAT,M.MTRMANFCTR,M.MTRMARK,M.REMARKS,M.MARKUPW,M.MARKUPR,M.PRICER,M.PRICEW,M.PRICEW01,M.PRICEW02,M.PRICEW03,M.PRICEW04,M.PRICEW05,M.PRICER01,M.PRICER02,M.PRICER03,M.PRICER04,M.PRICER05,M.NAME,M.NAME1,M.CODE,M.CODE1,M.CODE2,M.APVCODE,M.MTRPLACE,M.MTRSUP,M.MTRCATEGORY,M.MTRGROUP FROM MTRL M WHERE M.SODTYPE=51 AND MTRL = 421443";
         // echo "<BR>";
         //echo $params["fSQL"];
-        echo $params["fSQL"];
+       // echo $params["fSQL"];
         // echo "<BR>";
         //return;
         if ($this->getSetting("SoftoneBundle:Softone:apothiki") == 'carparts') {
             $params["fSQL"] = "SELECT * FROM MTRL M WHERE MTRL < 500";
+            echo $params["fSQL"];
             $softone = new Softone();
             $datas = $softone->createSql($params);  
             print_r($datas);
