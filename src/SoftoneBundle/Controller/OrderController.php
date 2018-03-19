880c84017c1c76df4423ebd300a484f1bf217191
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-19 11:35:11 +0200

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -967,12 +967,14 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
 
                         $sql = 'SELECT  ' . $this->select . ', p.reference, p.id
 									FROM ' . $this->repository . ' ' . $this->prefix . '
-									where p.itemCode1 = "' . $search[1] . '" OR p.itemIsactive = 1 AND (' . $qsupplier . '  (' . $tecdoc_article . $sqlearch2 . $tecdoc_article2 . ' ' . $this->prefix . '.id in (' . $sqlearch . ') OR ' . $sisxetisi . ') )
+									where p.itemCode1 = \'' . $search[1] . '\' OR p.itemIsactive = 1 AND (' . $qsupplier . '  (' . $tecdoc_article . $sqlearch2 . $tecdoc_article2 . ' ' . $this->prefix . '.id in (' . $sqlearch . ') OR ' . $sisxetisi . ') )
 									ORDER BY ' . $this->orderBy;
+                        /*
                         $sql = 'SELECT  ' . $this->select . ', p.reference, p.id
                                 FROM ' . $this->repository . ' ' . $this->prefix . '
                                 where p.itemCode1 = \'' . $search[1] . '\' OR p.itemIsactive = 1 AND (' . $qsupplier . ' ('.$tecdoc_article.' ' . $this->prefix . '.id in (' . $sqlearch . ') OR ' . $sisxetisi . '))
-                                ORDER BY ' . $this->orderBy;                        
+                                ORDER BY ' . $this->orderBy;
+                        */
                     }
                 } else {
                     $hasArticleIds = false;
