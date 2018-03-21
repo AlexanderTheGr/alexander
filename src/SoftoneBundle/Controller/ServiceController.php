b92dde8f24b1d20de212ed33ba19c732a1748817
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-21 16:05:24 +0200

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -54,7 +54,9 @@ class ServiceController extends Main{
         $q = str_replace("\n",",", $search);        
         $term = preg_replace("/[^a-zA-Z0-9]+/", "", $q[0]);
         $sql = "SELECT art.art_id as articleId FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."'))";			
+        echo $sql;
         $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql);
+        
         $datas = unserialize(file_get_contents($url));
 	print_r($datas);
         exit;
