1b3334fdabe2fef1e557dbdb17bd1d80d57bb253
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-21 16:02:35 +0200

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -56,7 +56,7 @@ class ServiceController extends Main{
         $sql = "SELECT art.art_id as articleId FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."'))";			
         $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql);
         $datas = unserialize(file_get_contents($url));
-	print_r($this->package($datas));
+	print_r($datas);
         exit;
         
         
