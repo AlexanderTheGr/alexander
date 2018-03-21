2ffeeb36cc4ca42534aaeab4928ab91827f9a32f
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-21 16:04:44 +0200

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -51,11 +51,10 @@ class ServiceController extends Main{
         //$data = $this->formLybase64();
         
         $search = $data["SoftoneBundle:Pcategory:itecategoryName:"];
-        $q = $items = str_replace("\n",",", $search);        
-        $term = preg_replace("/[^a-zA-Z0-9]+/", "", $q);
+        $q = str_replace("\n",",", $search);        
+        $term = preg_replace("/[^a-zA-Z0-9]+/", "", $q[0]);
         $sql = "SELECT art.art_id as articleId FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."'))";			
         $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql);
-        echo $sql;
         $datas = unserialize(file_get_contents($url));
 	print_r($datas);
         exit;
