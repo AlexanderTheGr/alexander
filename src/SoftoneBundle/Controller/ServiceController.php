77b106f68a2a095d7f3017374e158432bf399d18
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-23 12:10:13 +0200

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -55,14 +55,18 @@ class ServiceController extends Main{
         $items = explode("\n", $search);
         //print_r($items);    
         $term = preg_replace("/[^a-zA-Z0-9]+/", "", $items[0]);
-        $sql = "SELECT * FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."'))";			
-        $sql = "SELECT * FROM `articles` WHERE `art_id` in (SELECT `art_id` FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."')))";
-        //echo $sql;
-        //exit;
-        $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql);
+        //$sql = "SELECT * FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."'))";			
         
-        $datas = unserialize(file_get_contents($url));
-	print_r($datas);
+        $sql = "SELECT `str_id` FROM magento2_base4q2017.link_pt_str WHERE `str_type` = 1 AND pt_id in (Select pt_id from magento2_base4q2017.art_products_des where art_id = '".$art_id."')";
+
+        
+        foreach ($items as $term){
+            $term = preg_replace("/[^a-zA-Z0-9]+/", "", $term);
+            $sql = "SELECT * FROM `articles` WHERE `art_id` in (SELECT `art_id` FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."')))";
+            $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql); 
+            $datas = unserialize(file_get_contents($url));
+            print_r($datas);
+        }
         exit;
         
         
