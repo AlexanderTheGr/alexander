4a7fc04a2234810af529489faf6c0ccc352be0b8
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-23 12:40:30 +0200

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -63,7 +63,7 @@ class ServiceController extends Main{
         foreach ($items as $term){
             $term = preg_replace("/[^a-zA-Z0-9]+/", "", $term);
             //$sql = "SELECT * FROM `articles` WHERE `art_id` in (SELECT `art_id` FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."')))";
-            $sql = "SELECT * FROM `articles` where replace(replace(replace(replace(`art_article_nr`, '/', ''), '.', ''), '-', ''), ' ', '') LIKE '".$term."'";
+            $sql = "SELECT * FROM `articles` where art_article_nr_can LIKE '".$term."'";
             
             $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql); 
             $datas = unserialize(file_get_contents($url));
