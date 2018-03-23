3b2c8860ef93574da33abcb43129168aee65c1ec
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-23 15:59:16 +0200

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -139,10 +139,10 @@ class ServiceController extends Main{
                 $sup_id[$art_article_nr_can] = $terms[1];
             }
             
-            $sql = "SELECT atr_id, art_article_nr_can,sup_id,sup_brand FROM `articles`,suppliers where sup_id = art_sup_id AND art_article_nr_can in ('".implode("','",$art_article_nr_cans)."') order by sup_brand";
+            $sql = "SELECT art_id, art_article_nr_can,sup_id,sup_brand FROM `articles`,suppliers where sup_id = art_sup_id AND art_article_nr_can in ('".implode("','",$art_article_nr_cans)."') order by sup_brand";
             //$sql = "SELECT art_article_nr_can,sup_id,sup_brand FROM `articles`,suppliers where sup_id = art_sup_id AND `art_id` in (SELECT `art_id` FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."')))";
-            echo $sql;
-            exit;
+            //echo $sql;
+            //exit;
             $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql); 
             $datas = unserialize(file_get_contents($url));        
             
