3655b1f588b7d5be1ba05ff916772af48f5deca0
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-26 10:35:49 +0300

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -86,7 +86,15 @@ class ServiceController extends Main{
         $term = preg_replace("/[^a-zA-Z0-9]+/", "", $items[0]);
         //$sql = "SELECT * FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."'))";			
         $sql = "SELECT `str_id` FROM magento2_base4q2017.link_pt_str WHERE `str_type` = 1 AND pt_id in (Select pt_id from magento2_base4q2017.art_products_des where art_id = '".$art_id."')";
-       
+ 
+        if ($category > 0) {
+            $sql = "select * from cat2cat where w_str_id = '".$category."'";
+            $connection = $em->getConnection();
+            $statement = $connection->prepare($sql);
+            $statement->execute();
+            $results = $statement->fetchAll();
+            print_r($results);
+        }
         
         $html = $this->$type((array)$items);
         $json = json_encode(array("ok", "html" => $html,'divid'=>"resulthtml"));
