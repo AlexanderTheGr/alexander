4043e9c676c5fc1efb2561644dfe18183fbbad2b
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-27 15:45:04 +0300

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1858,11 +1858,22 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
     public function getUpdateTecdocAction($funct = false) {
         $em = $this->getDoctrine()->getManager();
 
-        $query = $em->createQuery(
-                "SELECT  p.id
-                    FROM " . $this->repository . " p
-                    where p.tecdocSupplierId > 0 AND p.tecdocArticleId IS NULL order by p.id desc"
-        );
+
+        if ($this->getSetting("AppBundle:Entity:newTecdocServiceUrl") != '') { 
+            $query = $em->createQuery(
+                    "SELECT  p.id
+                        FROM " . $this->repository . " p
+                        where p.tecdocSupplierId > 0 AND p.tecdocArticleId > 0 order by p.id asc"
+            );            
+        } else {
+            $query = $em->createQuery(
+                    "SELECT  p.id
+                        FROM " . $this->repository . " p
+                        where p.tecdocSupplierId > 0 AND p.tecdocArticleId IS NULL order by p.id asc"
+            );
+        }        
+        
+        
         /*
           $query = $em->createQuery(
           "SELECT  p.id
