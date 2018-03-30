f770e2b0a5e6430f4ef95fc60a7c4b95b798e182
AlexanderTheGr <a.dimeas@gmail.com>
2018-03-30 13:52:22 +0300

    ...

--- src/EdiBundle/Controller/EdiItemController.php
+++ src/EdiBundle/Controller/EdiItemController.php
@@ -533,8 +533,8 @@ class EdiItemController extends Main {
                         //$this->where = " where " . $this->prefix . ".Edi = '" . $edi . "' AND " . $this->prefix . ".artNr != '' AND " . $this->prefix . ".partno != '' AND ((" . $this->prefix . ".tecdocArticleId3 in (" . (implode(",", $articleIds)) . ") OR " . $this->prefix . ".partno = '" . $search[1] . "' OR " . $this->prefix . ".artNr = '" . $search[1] . "' OR " . $this->prefix . ".itemCode = '" . $search[1] . "'))";
                     }
 
-                    echo $this->where;
-                    exit;
+                    //echo $this->where;
+                    //exit;
                 } else {
                     if ($search11[0] == 'productfano') {
                         $this->where = " where " . $this->prefix . ".itemCode LIKE '" . $search[1] . "%'))";
