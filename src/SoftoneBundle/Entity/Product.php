67e37d48710847e6ae38fbeb5bf81663bce706b2
AlexanderTheGr <a.dimeas@gmail.com>
2018-04-02 15:47:35 +0300

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -3357,7 +3357,7 @@ class Product extends Entity {
 				cri_id = lac_cri_id and 
 				lac_gr_id = lat_lac_gr_id and
 				des_id = cri_des_id and 
-				(des_lng_id = '" . $this->lng . "' OR des_lng_id = 4) AND
+				(des_lng_id = '20' OR des_lng_id = 4) AND
 				des_text != '' AND
 				lat_typ_id = '" . $brandmodeltype . "' AND 
 				lat_art_id = '" . $this->tecdocArticleId . "' order by des_lng_id";
