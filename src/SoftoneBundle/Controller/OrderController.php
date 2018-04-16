23cab36a0d8a95b9e612cd8f0838386939b91651
AlexanderTheGr <a.dimeas@gmail.com>
2018-04-16 11:12:47 +0300

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -723,14 +723,14 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
             if ($search[0] != 'productfano') {
                 if ($this->getSetting("AppBundle:Entity:newTecdocServiceUrl") != '') {
                     $articleIds = array();
-                    $term = preg_replace("/[^a-zA-Z0-9]+/", "", $params["search"]);
+                    $term = preg_replace("/[^a-zA-Z0-9]+/", "", $search[1]);
                     $sql = "SELECT art.art_id as articleId FROM magento2_base4q2017.articles art WHERE (art.art_id in (SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '".$term."'))";			
                     $url = "http://magento2.fastwebltd.com/service.php?sql=".base64_encode($sql);
                     $datas = unserialize(file_get_contents($url)); 
                     foreach($datas as $data) {
                         $articleIds[] = $data["articleId"]; 
                     }
-                    print_r($articleIds);
+                    //print_r($articleIds);
                 } else {
                     $articleIds = (array) unserialize($this->getArticlesSearch($this->clearstring($search[1])));
                 }
