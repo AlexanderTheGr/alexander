08a2b787ddedd880385cb5680d6b7397ced626c4
AlexanderTheGr <a.dimeas@gmail.com>
2018-04-27 14:24:04 +0300

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -422,6 +422,7 @@ class ServiceController extends Main {
 
             
             foreach ($datas as $data) {
+                print_r($data);
                 $out[$data["art_article_nr_can"]] = $data;
             }
             $html = $tecdocSupplierId . " -- " . $brand . '<BR><table>';
