46174e7bac087733566a39e94962f43c44fad6d2
AlexanderTheGr <a.dimeas@gmail.com>
2018-05-08 14:45:17 +0300

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -261,6 +261,7 @@ class ServiceController extends Main {
                 $terms = explode("\t", $term);
                 $art_article_nr_can = preg_replace("/[^a-zA-Z0-9]+/", "", $terms[0]);
                 $art_article_nr_cans[] = $art_article_nr_can;
+                $terms[1] = $terms[1] > 0 ? $terms[1] : $tecdocSupplierId; 
                 $sup_id[$art_article_nr_can] = $terms[1];
                 $out[$art_article_nr_can] = array();
             }
