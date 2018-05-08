a63f2647b1fa79f9ef2067c787c32471ec15791b
AlexanderTheGr <a.dimeas@gmail.com>
2018-05-08 11:42:48 +0300

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -216,9 +216,9 @@ class ServiceController extends Main {
                     //$sql = "Select mod_lnk_vich_id from magento2_base4q2017.art_mod_links a, magento2_base4q2017.models_links b where `mod_lnk_type` = 1 AND a.mod_lnk_id = b.mod_lnk_id and art_id = '" . $data["art_id"] . "' group by `mod_lnk_vich_id`";
                     //$url = "http://magento2.fastwebltd.com/service.php?sql=" . base64_encode($sql);
                     //$models = unserialize($this->curlit($url, "sql=" . base64_encode($sql)));
-                    $mdo = array();
+                    //$mdo = array();
                     //foreach ($models as $model_type) {
-                    $mdo[$data["art_article_nr_can"]] = $model_type["mod_lnk_vich_id"];
+                    $mdo[$data["art_article_nr_can"]] = $data["mod_lnk_vich_id"];
                     $out[$data["art_article_nr_can"]][3] = count($mdo[$data["art_article_nr_can"]] );
                     $out[$data["art_article_nr_can"]][4] = implode(",", $mdo[$data["art_article_nr_can"]]);
                 } else {
