e6353cc527faf114e6895337ff560c85a300c1a8
AlexanderTheGr <a.dimeas@gmail.com>
2018-05-15 13:15:24 +0300

    ...

--- src/SoftoneBundle/Entity/Customer.php
+++ src/SoftoneBundle/Entity/Customer.php
@@ -1041,8 +1041,7 @@ class Customer extends Entity {
         //@$dataOut["ITEEXTRA"][0] = array("NUM02" => $this->item_mtrl_iteextra_num02);
         //print_r(@$dataOut);
         $out = $softone->setData((array) $dataOut, $object, (int) $this->reference);
-        //print_r($out);
-        
+        print_r($out);
         if (@$out->id > 1) {
             $filters = "CUSTOMER.CODE=" . $this->customerCode . "&CUSTOMER.CODE_TO=" . $this->customerCode;
             $datas = $softone->retrieveData($object, "partsbox", $filters);
