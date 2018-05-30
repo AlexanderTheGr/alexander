b722ccc1aafa76e5d27ec377a083a874eb4fc886
AlexanderTheGr <a.dimeas@gmail.com>
2018-05-30 10:07:05 +0300

    ...

--- src/SoftoneBundle/Controller/ServiceController.php
+++ src/SoftoneBundle/Controller/ServiceController.php
@@ -639,7 +639,10 @@ class ServiceController extends Main {
                 if ($criteria["cri_id"] == 20 OR $criteria["cri_id"] == 21)
                     $criteria["value"] = str_replace(".", "/", $criteria["value"]);
                 //$out .= "<li><b>".$criteria["name"]." [".$criteria["cri_id"]."]</b>:: ".mb_convert_case($criteria["value"], MB_CASE_TITLE, "UTF-8")."</li>";
-                $out .= "<li><b>" . @mb_convert_case($criteria["name"], MB_CASE_TITLE, "UTF-8") . "</b>: " . @mb_convert_case($criteria["value"], MB_CASE_TITLE, "UTF-8") . "</li>";
+                //$out .= "<li><b>" . @mb_convert_case($criteria["name"], MB_CASE_TITLE, "UTF-8") . "</b>: " . @mb_convert_case($criteria["value"], MB_CASE_TITLE, "UTF-8") . "</li>";
+                $out .= "<li><b>" .$criteria["name"] . "</b>: " . $criteria["value"] . "</li>";
+            
+                
             }
             $out .= "<ul>";
             //print_r($criterias);
