8847c8ab1bc7a51ea390039ad1d7dd0586db86f8
AlexanderTheGr <a.dimeas@gmail.com>
2018-06-27 15:33:16 +0300

    ...

--- src/SoftoneBundle/Controller/ProductController.php
+++ src/SoftoneBundle/Controller/ProductController.php
@@ -1471,10 +1471,10 @@ class ProductController extends \SoftoneBundle\Controller\SoftoneController {
 
                     if ($this->getSetting("AppBundle:Entity:newTecdocServiceUrl") != '') {
                         $MTRL1 = 1;
-                        $MTRL2 = 2000;                        
+                        $MTRL2 = 12000;                        
                         $where = " AND  MTRL >= " . $MTRL1 . " AND MTRL < " . $MTRL2 . "  ORDER BY MTRL";
                         //$where = " AND UPDDATE >= '" . date("Y-m-d", strtotime("-1000 days")) . "' ORDER BY MTRL";
-                        
+                        echo 'ssssss';
                     } else {
                         $where = " AND UPDDATE >= '" . date("Y-m-d", strtotime("-1 days")) . "' ORDER BY MTRL";
                     }
