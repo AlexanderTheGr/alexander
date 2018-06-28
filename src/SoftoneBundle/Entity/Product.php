6974dae82cebbb534668b51ed44ed5b706878576
AlexanderTheGr <a.dimeas@gmail.com>
2018-06-28 12:31:01 +0300

    ...

--- src/SoftoneBundle/Entity/Product.php
+++ src/SoftoneBundle/Entity/Product.php
@@ -2054,7 +2054,7 @@ class Product extends Entity {
                 if (in_array($cat["w_str_id"], $array)) {
                     $del = true;
                     if ($kv != 'VA' AND $kv != 'HA' AND $kv != 'VL' AND $kv != 'HL' AND $kv != 'VR' AND $kv != 'HR' AND $kv != 'V' AND $kv != 'H') {
-                        $term = preg_replace("/[^a-zA-Z0-9]+/", "", $product->getTecdocCode());
+                        $term = preg_replace("/[^a-zA-Z0-9]+/", "", $this->tecdocCode);
                         $sql = "SELECT all_art_id FROM magento2_base4q2017.art_lookup_links, magento2_base4q2017.art_lookup where all_arl_id = arl_id and arl_search_number = '" . $term . "'";
                         //$arts = $this->connection->fetchAll($sql);
                         //$result = mysqli_query($this->conn,$sql);
