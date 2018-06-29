9e97a0d22ebf9a1a1dc15d14f9a9bb9f9b82ada6
AlexanderTheGr <a.dimeas@gmail.com>
2018-06-29 09:57:46 +0300

    ...

--- src/SoftoneBundle/Controller/OrderController.php
+++ src/SoftoneBundle/Controller/OrderController.php
@@ -517,7 +517,7 @@ class OrderController extends \SoftoneBundle\Controller\SoftoneController {
         if ($_SERVER["REMOTE_ADDR"] == '212.205.224.191') {
             if ($entity->getId()) {
                 $this->addTab(array("title" => $this->getTranslation("Search"), "datatables" => array(), "form" => '', "content" => $this->getTabContentSearch($entity), "index" => $this->generateRandomString(), 'search' => 'text', "active" => true));
-                //$this->addTab(array("title" => $this->getTranslation("Καλάθι"), "datatables" => $datatables, "form" => '', "content" => "", "index" => $this->generateRandomString(), 'search' => 'text', "active" => false));
+                $this->addTab(array("title" => $this->getTranslation("Καλάθι"), "datatables" => $datatables, "form" => '', "content" => "", "index" => $this->generateRandomString(), 'search' => 'text', "active" => false));
                 //$this->addTab(array("title" => $this->getTranslation("Customer Details"), "datatables" => array(), "form" => '', "content" => $this->getCustomerDetails($entity), "index" => $this->generateRandomString(), 'search' => 'text', "active" => false));
             }            
         } else {    
